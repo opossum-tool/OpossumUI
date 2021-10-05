ccd906cdefd29094a26ae0521d2256c4735b9e48
Anton Bauhofer <anton.bauhofer@tngtech.com>
2021-10-05 18:03:12 +0200

    Add missing sources with priorities for pretty printing
    Signed-off-by: Anton Bauhofer <anton.bauhofer@tngtech.com>

--- src/shared/shared-constants.ts
+++ src/shared/shared-constants.ts
@@ -7,13 +7,14 @@ import { ExternalAttributionSources } from './shared-types';
 
 // TODO: retire this list
 export const ATTRIBUTION_SOURCES: ExternalAttributionSources = {
-  MERGER: { name: 'Suggested', priority: 10 },
-  HHC: { name: 'High High Compute', priority: 9 },
-  MS: { name: 'Metadata Scanner', priority: 8 },
-  'REUSER:HHC': { name: 'High High Compute (old scan)', priority: 7 },
-  'REUSER:MS': { name: 'Metadata Scanner (old scan)', priority: 6 },
-  'REUSER:SC': { name: 'ScanCode (old scan)', priority: 5 },
-  'REUSER:HC': { name: 'High Compute (old scan)', priority: 4 },
+  MERGER: { name: 'Suggested', priority: 11 },
+  HHC: { name: 'High High Compute', priority: 10 },
+  MS: { name: 'Metadata Scanner', priority: 9 },
+  'REUSER:HHC': { name: 'High High Compute (old scan)', priority: 8 },
+  'REUSER:MS': { name: 'Metadata Scanner (old scan)', priority: 7 },
+  'REUSER:SC': { name: 'ScanCode (old scan)', priority: 6 },
+  'REUSER:HC': { name: 'High Compute (old scan)', priority: 5 },
+  'REUSER:MERGER': { name: 'Suggested (old scan)', priority: 4 },
   SC: { name: 'ScanCode', priority: 3 },
   HC: { name: 'High Compute', priority: 2 },
   HINT: { name: 'Hint', priority: 1 },
