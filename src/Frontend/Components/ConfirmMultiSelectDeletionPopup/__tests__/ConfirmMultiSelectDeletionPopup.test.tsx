1e664368da0df62e4dc938d16d2e9459a1efc5ae
Jonas Tai <jonas.tai@tngtech.com>
2021-12-22 15:07:44 +0100

    Add unit test for ConfirmMultiSelectDeletionPopup
    Adds a test that tests the deletion functionality of the ConfirmMultiSelectDeletionPopup.
    
    Signed-off-by: Jonas Tai <jonas.tai@tngtech.com>

--- src/Frontend/Components/ConfirmMultiSelectDeletionPopup/__tests__/ConfirmMultiSelectDeletionPopup.test.tsx
+++ src/Frontend/Components/ConfirmMultiSelectDeletionPopup/__tests__/ConfirmMultiSelectDeletionPopup.test.tsx
@@ -3,7 +3,10 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { renderComponentWithStore } from '../../../test-helpers/render-component-with-store';
+import {
+  createTestAppStore,
+  renderComponentWithStore,
+} from '../../../test-helpers/render-component-with-store';
 import { screen } from '@testing-library/react';
 import React from 'react';
 import { ConfirmMultiSelectDeletionPopup } from '../ConfirmMultiSelectDeletionPopup';
@@ -11,8 +14,39 @@ import {
   setMultiSelectMode,
   setMultiSelectSelectedAttributionIds,
 } from '../../../state/actions/resource-actions/attribution-view-simple-actions';
+import {
+  Attributions,
+  Resources,
+  ResourcesToAttributions,
+} from '../../../../shared/shared-types';
+import { loadFromFile } from '../../../state/actions/resource-actions/load-actions';
+import {
+  clickOnButton,
+  getParsedInputFileEnrichedWithTestData,
+} from '../../../test-helpers/general-test-helpers';
+import { ButtonText } from '../../../enums/enums';
+import { getManualAttributions } from '../../../state/selectors/all-views-resource-selectors';
+import { IpcRenderer } from 'electron';
+
+let originalIpcRenderer: IpcRenderer;
 
 describe('The ConfirmMultiSelectDeletionPopup', () => {
+  beforeAll(() => {
+    originalIpcRenderer = global.window.ipcRenderer;
+    global.window.ipcRenderer = {
+      on: jest.fn(),
+      removeListener: jest.fn(),
+      invoke: jest.fn(),
+    } as unknown as IpcRenderer;
+  });
+
+  beforeEach(() => jest.clearAllMocks());
+
+  afterAll(() => {
+    // Important to restore the original value.
+    global.window.ipcRenderer = originalIpcRenderer;
+  });
+
   test('renders', () => {
     const expectedContent =
       'Do you really want to delete the selected attributions for all files? This action will delete 2 attributions.';
@@ -26,5 +60,55 @@ describe('The ConfirmMultiSelectDeletionPopup', () => {
 
     expect(screen.getByText(expectedContent)).toBeTruthy();
     expect(screen.getByText(expectedHeader)).toBeTruthy();
+  });
+
+  test('deletes attributions', () => {
+    const expectedContent =
+      'Do you really want to delete the selected attributions for all files? This action will delete 2 attributions.';
+    const expectedHeader = 'Confirm Deletion';
+
+    const testResources: Resources = {
+      'something.js': 1,
+      'somethingElse.js': 1,
+    };
+    const testManualAttributions: Attributions = {
+      uuid1: {
+        packageName: 'React',
+      },
+      uuid2: {
+        packageName: 'Vue',
+      },
+    };
+    const testResourcesToManualAttributions: ResourcesToAttributions = {
+      '/something.js': ['uuid1'],
+      '/somethingElse.js': ['uuid2'],
+    };
+
+    const testStore = createTestAppStore();
+    testStore.dispatch(
+      loadFromFile(
+        getParsedInputFileEnrichedWithTestData({
+          resources: testResources,
+          manualAttributions: testManualAttributions,
+          resourcesToManualAttributions: testResourcesToManualAttributions,
+        })
+      )
+    );
+    testStore.dispatch(setMultiSelectMode(true));
+    testStore.dispatch(
+      setMultiSelectSelectedAttributionIds(['uuid1', 'uuid2'])
+    );
+
+    const { store } = renderComponentWithStore(
+      <ConfirmMultiSelectDeletionPopup />,
+      { store: testStore }
+    );
+
+    store.dispatch(setMultiSelectMode(true));
+    store.dispatch(setMultiSelectSelectedAttributionIds(['uuid1', 'uuid2']));
+    expect(screen.getByText(expectedContent)).toBeTruthy();
+    expect(screen.getByText(expectedHeader)).toBeTruthy();
+    clickOnButton(screen, ButtonText.Confirm);
+    expect(getManualAttributions(store.getState())).toEqual({});
   });
 });
