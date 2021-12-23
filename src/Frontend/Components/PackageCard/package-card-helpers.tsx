56f55867b9823fb8238f808c046eaae74c422dbb
Benedikt Richter <benedikt.richter@tngtech.com>
2021-12-23 11:00:44 +0100

    Refactor
    Refactor PackageCard and move some functions to helpers file.
    
    Signed-off-by: Benedikt Richter <benedikt.richter@tngtech.com>

---
+++ src/Frontend/Components/PackageCard/package-card-helpers.tsx
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: Facebook, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { ListCardConfig, ListCardContent } from '../../types/types';
+import { ClassNameMap } from '@mui/styles';
+import React, { ReactElement } from 'react';
+import {
+  ExcludeFromNoticeIcon,
+  FirstPartyIcon,
+  FollowUpIcon,
+  PreSelectedIcon,
+} from '../Icons/Icons';
+
+export function getKey(prefix: string, cardContent: ListCardContent): string {
+  return `${prefix}-${cardContent.id}`;
+}
+
+export function getRightIcons(
+  cardContent: ListCardContent,
+  cardConfig: ListCardConfig,
+  classes: ClassNameMap<'followUpIcon' | 'excludeFromNoticeIcon'>
+): Array<ReactElement> {
+  const rightIcons: Array<JSX.Element> = [];
+
+  if (cardConfig.firstParty) {
+    rightIcons.push(
+      <FirstPartyIcon key={getKey('first-party-icon', cardContent)} />
+    );
+  }
+  if (cardConfig.excludeFromNotice) {
+    rightIcons.push(
+      <ExcludeFromNoticeIcon
+        key={getKey('exclude-icon', cardContent)}
+        className={classes.excludeFromNoticeIcon}
+      />
+    );
+  }
+  if (cardConfig.followUp) {
+    rightIcons.push(
+      <FollowUpIcon
+        key={getKey('follow-up-icon', cardContent)}
+        className={classes.followUpIcon}
+      />
+    );
+  }
+  if (cardConfig.isPreSelected) {
+    rightIcons.push(
+      <PreSelectedIcon key={getKey('pre-selected-icon', cardContent)} />
+    );
+  }
+
+  return rightIcons;
+}
