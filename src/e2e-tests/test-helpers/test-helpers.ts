9cfc78a7b66053ae7521419757551d38942b408d
Benedikt Richter <benedikt.richter@tngtech.com>
2022-02-09 10:27:36 +0100

    Increase e2e test timeouts
    Some e2e test did time out in CI.
    
    Signed-off-by: Benedikt Richter <benedikt.richter@tngtech.com>

--- src/e2e-tests/test-helpers/test-helpers.ts
+++ src/e2e-tests/test-helpers/test-helpers.ts
@@ -6,8 +6,8 @@
 import { _electron, ElectronApplication } from 'playwright';
 
 const ELECTRON_LAUNCH_TEST_TIMEOUT = 60000;
-export const E2E_TEST_TIMEOUT = 40000;
-export const E2E_LARGE_TEST_TIMEOUT = 480000;
+export const E2E_TEST_TIMEOUT = 50000;
+export const E2E_LARGE_TEST_TIMEOUT = 600000;
 
 export async function getApp(
   commandLineArg?: string
