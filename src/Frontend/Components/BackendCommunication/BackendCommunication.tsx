dd8c99c04d7e8307d4cf74bbca60ce17bb8d292d
Benedikt Richter <benedikt.richter@tngtech.com>
2022-03-28 15:56:13 +0200

    Extract backend listeners
    The communication with the electron backend is extracted to an own
    component in order to better separate the logic of the top bar
    from the listener logic.
    
    Signed-off-by: Benedikt Richter <benedikt.richter@tngtech.com>

---
+++ src/Frontend/Components/BackendCommunication/BackendCommunication.tsx
@@ -0,0 +1,274 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { IpcRendererEvent } from 'electron';
+import { ReactElement } from 'react';
+import pick from 'lodash/pick';
+import { useAppDispatch, useAppSelector } from '../../state/hooks';
+import { IpcChannel } from '../../../shared/ipc-channels';
+import {
+  Attributions,
+  BaseURLForRootArgs,
+  ExportSpdxDocumentJsonArgs,
+  ExportSpdxDocumentYamlArgs,
+  ExportType,
+  ParsedFileContent,
+  ToggleHighlightForCriticalSignalsArgs,
+} from '../../../shared/shared-types';
+import { PopupType } from '../../enums/enums';
+import {
+  resetResourceState,
+  setBaseUrlsForSources,
+} from '../../state/actions/resource-actions/all-views-simple-actions';
+import { loadFromFile } from '../../state/actions/resource-actions/load-actions';
+import {
+  getAttributionBreakpoints,
+  getBaseUrlsForSources,
+  getFilesWithChildren,
+  getFrequentLicensesTexts,
+  getManualData,
+  getResources,
+} from '../../state/selectors/all-views-resource-selectors';
+import { getHighlightForCriticalSignals } from '../../state/selectors/view-selector';
+import {
+  getAttributionsWithAllChildResourcesWithoutFolders,
+  getAttributionsWithResources,
+  removeSlashesFromFilesWithChildren,
+} from '../../util/get-attributions-with-resources';
+import { useIpcRenderer } from '../../util/use-ipc-renderer';
+import { getAttributionBreakpointCheck } from '../../util/is-attribution-breakpoint';
+import { getFileWithChildrenCheck } from '../../util/is-file-with-children';
+import {
+  openPopup,
+  setHighlightForCriticalSignals,
+} from '../../state/actions/view-actions/view-actions';
+
+export function BackendCommunication(): ReactElement | null {
+  const resources = useAppSelector(getResources);
+  const manualData = useAppSelector(getManualData);
+  const attributionBreakpoints = useAppSelector(getAttributionBreakpoints);
+  const filesWithChildren = useAppSelector(getFilesWithChildren);
+  const frequentLicenseTexts = useAppSelector(getFrequentLicensesTexts);
+  const baseUrlsForSources = useAppSelector(getBaseUrlsForSources);
+  const showHighlightForCriticalSignals = useAppSelector(
+    getHighlightForCriticalSignals
+  );
+  const dispatch = useAppDispatch();
+
+  function fileLoadedListener(
+    event: IpcRendererEvent,
+    parsedFileContent: ParsedFileContent
+  ): void {
+    dispatch(loadFromFile(parsedFileContent));
+  }
+
+  function getExportFileRequestListener(
+    event: IpcRendererEvent,
+    exportType: ExportType
+  ): void {
+    switch (exportType) {
+      case ExportType.SpdxDocumentJson:
+      case ExportType.SpdxDocumentYaml:
+        return getSpdxDocumentExportListener(exportType);
+      case ExportType.FollowUp:
+        return getFollowUpExportListener();
+      case ExportType.CompactBom:
+        return getCompactBomExportListener();
+      case ExportType.DetailedBom:
+        return getDetailedBomExportListener();
+    }
+  }
+
+  function getFollowUpExportListener(): void {
+    const followUpAttributions = pick(
+      manualData.attributions,
+      Object.keys(manualData.attributions).filter(
+        (attributionId) => manualData.attributions[attributionId].followUp
+      )
+    );
+
+    const followUpAttributionsWithResources =
+      getAttributionsWithAllChildResourcesWithoutFolders(
+        followUpAttributions,
+        manualData.attributionsToResources,
+        manualData.resourcesToAttributions,
+        resources || {},
+        getAttributionBreakpointCheck(attributionBreakpoints),
+        getFileWithChildrenCheck(filesWithChildren)
+      );
+    const followUpAttributionsWithFormattedResources =
+      removeSlashesFromFilesWithChildren(
+        followUpAttributionsWithResources,
+        getFileWithChildrenCheck(filesWithChildren)
+      );
+
+    window.ipcRenderer.invoke(IpcChannel.ExportFile, {
+      type: ExportType.FollowUp,
+      followUpAttributionsWithResources:
+        followUpAttributionsWithFormattedResources,
+    });
+  }
+
+  function getSpdxDocumentExportListener(
+    exportType: ExportType.SpdxDocumentYaml | ExportType.SpdxDocumentJson
+  ): void {
+    const attributions = Object.fromEntries(
+      Object.entries(manualData.attributions).map((entry) => {
+        const packageInfo = entry[1];
+
+        const licenseName = packageInfo.licenseName || '';
+        const isFrequentLicense =
+          licenseName && licenseName in frequentLicenseTexts;
+        const licenseText =
+          packageInfo.licenseText || isFrequentLicense
+            ? frequentLicenseTexts[licenseName]
+            : '';
+        return [
+          entry[0],
+          {
+            ...entry[1],
+            licenseText,
+          },
+        ];
+      })
+    );
+
+    const args: ExportSpdxDocumentYamlArgs | ExportSpdxDocumentJsonArgs = {
+      type: exportType,
+      spdxAttributions: attributions,
+    };
+
+    window.ipcRenderer.invoke(IpcChannel.ExportFile, args);
+  }
+
+  function getDetailedBomExportListener(): void {
+    const bomAttributions = getBomAttributions(manualData.attributions);
+
+    const bomAttributionsWithResources = getAttributionsWithResources(
+      bomAttributions,
+      manualData.attributionsToResources
+    );
+
+    const bomAttributionsWithFormattedResources =
+      removeSlashesFromFilesWithChildren(
+        bomAttributionsWithResources,
+        getFileWithChildrenCheck(filesWithChildren)
+      );
+
+    window.ipcRenderer.invoke(IpcChannel.ExportFile, {
+      type: ExportType.DetailedBom,
+      bomAttributionsWithResources: bomAttributionsWithFormattedResources,
+    });
+  }
+
+  function getCompactBomExportListener(): void {
+    window.ipcRenderer.invoke(IpcChannel.ExportFile, {
+      type: ExportType.CompactBom,
+      bomAttributions: getBomAttributions(manualData.attributions),
+    });
+  }
+
+  function getBomAttributions(attributions: Attributions): Attributions {
+    return pick(
+      attributions,
+      Object.keys(attributions).filter(
+        (attributionId) =>
+          !attributions[attributionId].followUp &&
+          !attributions[attributionId].firstParty
+      )
+    );
+  }
+
+  function resetLoadedFileListener(
+    event: IpcRendererEvent,
+    resetState: boolean
+  ): void {
+    if (resetState) {
+      dispatch(resetResourceState());
+    }
+  }
+
+  function loggingListener(event: IpcRendererEvent, logging: string): void {
+    if (logging) {
+      console.log(logging);
+    }
+  }
+
+  function showSearchPopupListener(
+    event: IpcRendererEvent,
+    showSearchPopUp: boolean
+  ): void {
+    if (showSearchPopUp) {
+      dispatch(openPopup(PopupType.FileSearchPopup));
+    }
+  }
+
+  function showProjectMetadataPopupListener(
+    event: IpcRendererEvent,
+    showProjectMetadataPopup: boolean
+  ): void {
+    if (showProjectMetadataPopup) {
+      dispatch(openPopup(PopupType.ProjectMetadataPopup));
+    }
+  }
+
+  function setBaseURLForRootListener(
+    event: IpcRendererEvent,
+    baseURLForRootArgs: BaseURLForRootArgs
+  ): void {
+    if (baseURLForRootArgs?.baseURLForRoot) {
+      dispatch(
+        setBaseUrlsForSources({
+          ...baseUrlsForSources,
+          '/': baseURLForRootArgs.baseURLForRoot,
+        })
+      );
+    }
+  }
+
+  function setToggleHighlightForCriticalSignalsListener(
+    event: IpcRendererEvent,
+    toggleHighlightForCriticalSignalsArgs: ToggleHighlightForCriticalSignalsArgs
+  ): void {
+    if (
+      toggleHighlightForCriticalSignalsArgs?.toggleHighlightForCriticalSignals
+    ) {
+      dispatch(
+        setHighlightForCriticalSignals(!showHighlightForCriticalSignals)
+      );
+    }
+  }
+
+  useIpcRenderer(IpcChannel.FileLoaded, fileLoadedListener, [dispatch]);
+  useIpcRenderer(IpcChannel.ResetLoadedFile, resetLoadedFileListener, [
+    dispatch,
+  ]);
+  useIpcRenderer(IpcChannel.Logging, loggingListener, [dispatch]);
+  useIpcRenderer(IpcChannel.ShowSearchPopup, showSearchPopupListener, [
+    dispatch,
+  ]);
+  useIpcRenderer(
+    IpcChannel.ShowProjectMetadataPopup,
+    showProjectMetadataPopupListener,
+    [dispatch]
+  );
+  useIpcRenderer(IpcChannel.SetBaseURLForRoot, setBaseURLForRootListener, [
+    dispatch,
+    baseUrlsForSources,
+  ]);
+  useIpcRenderer(IpcChannel.ExportFileRequest, getExportFileRequestListener, [
+    manualData,
+    attributionBreakpoints,
+    frequentLicenseTexts,
+    filesWithChildren,
+  ]);
+  useIpcRenderer(
+    IpcChannel.ToggleHighlightForCriticalSignals,
+    setToggleHighlightForCriticalSignalsListener,
+    [dispatch, showHighlightForCriticalSignals]
+  );
+
+  return null;
+}
--- src/Frontend/Components/TopBar/TopBar.tsx
+++ src/Frontend/Components/TopBar/TopBar.tsx
@@ -6,57 +6,18 @@
 import makeStyles from '@mui/styles/makeStyles';
 import MuiToggleButton from '@mui/material/ToggleButton';
 import MuiToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { IpcRendererEvent } from 'electron';
-import pick from 'lodash/pick';
 import React, { ReactElement } from 'react';
 import { useAppDispatch, useAppSelector } from '../../state/hooks';
 import { IpcChannel } from '../../../shared/ipc-channels';
-import {
-  Attributions,
-  BaseURLForRootArgs,
-  ExportSpdxDocumentJsonArgs,
-  ExportSpdxDocumentYamlArgs,
-  ExportType,
-  ParsedFileContent,
-  ToggleHighlightForCriticalSignalsArgs,
-} from '../../../shared/shared-types';
-import { PopupType, View } from '../../enums/enums';
+import { View } from '../../enums/enums';
 import { setViewOrOpenUnsavedPopup } from '../../state/actions/popup-actions/popup-actions';
-import {
-  resetResourceState,
-  setBaseUrlsForSources,
-} from '../../state/actions/resource-actions/all-views-simple-actions';
-import { loadFromFile } from '../../state/actions/resource-actions/load-actions';
-import {
-  getAttributionBreakpoints,
-  getBaseUrlsForSources,
-  getFilesWithChildren,
-  getFrequentLicensesTexts,
-  getManualData,
-  getResources,
-} from '../../state/selectors/all-views-resource-selectors';
-import {
-  getHighlightForCriticalSignals,
-  getSelectedView,
-} from '../../state/selectors/view-selector';
-import {
-  getAttributionsWithAllChildResourcesWithoutFolders,
-  getAttributionsWithResources,
-  removeSlashesFromFilesWithChildren,
-} from '../../util/get-attributions-with-resources';
-import { useIpcRenderer } from '../../util/use-ipc-renderer';
+import { getSelectedView } from '../../state/selectors/view-selector';
 import { CommitInfoDisplay } from '../CommitInfoDisplay/CommitInfoDisplay';
 import { ProgressBar } from '../ProgressBar/ProgressBar';
 import { OpossumColors } from '../../shared-styles';
-
-import { getAttributionBreakpointCheck } from '../../util/is-attribution-breakpoint';
-import { getFileWithChildrenCheck } from '../../util/is-file-with-children';
-import {
-  openPopup,
-  setHighlightForCriticalSignals,
-} from '../../state/actions/view-actions/view-actions';
 import { IconButton } from '../IconButton/IconButton';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import { BackendCommunication } from '../BackendCommunication/BackendCommunication';
 
 export const topBarHeight = 36;
 
@@ -100,230 +61,8 @@ const useStyles = makeStyles({
 export function TopBar(): ReactElement {
   const classes = useStyles();
 
-  const resources = useAppSelector(getResources);
-  const manualData = useAppSelector(getManualData);
   const selectedView = useAppSelector(getSelectedView);
-  const attributionBreakpoints = useAppSelector(getAttributionBreakpoints);
-  const filesWithChildren = useAppSelector(getFilesWithChildren);
-  const frequentLicenseTexts = useAppSelector(getFrequentLicensesTexts);
-  const baseUrlsForSources = useAppSelector(getBaseUrlsForSources);
-  const showHighlightForCriticalSignals = useAppSelector(
-    getHighlightForCriticalSignals
-  );
   const dispatch = useAppDispatch();
-
-  function fileLoadedListener(
-    event: IpcRendererEvent,
-    parsedFileContent: ParsedFileContent
-  ): void {
-    dispatch(loadFromFile(parsedFileContent));
-  }
-
-  function getExportFileRequestListener(
-    event: IpcRendererEvent,
-    exportType: ExportType
-  ): void {
-    switch (exportType) {
-      case ExportType.SpdxDocumentJson:
-      case ExportType.SpdxDocumentYaml:
-        return getSpdxDocumentExportListener(exportType);
-      case ExportType.FollowUp:
-        return getFollowUpExportListener();
-      case ExportType.CompactBom:
-        return getCompactBomExportListener();
-      case ExportType.DetailedBom:
-        return getDetailedBomExportListener();
-    }
-  }
-
-  function getFollowUpExportListener(): void {
-    const followUpAttributions = pick(
-      manualData.attributions,
-      Object.keys(manualData.attributions).filter(
-        (attributionId) => manualData.attributions[attributionId].followUp
-      )
-    );
-
-    const followUpAttributionsWithResources =
-      getAttributionsWithAllChildResourcesWithoutFolders(
-        followUpAttributions,
-        manualData.attributionsToResources,
-        manualData.resourcesToAttributions,
-        resources || {},
-        getAttributionBreakpointCheck(attributionBreakpoints),
-        getFileWithChildrenCheck(filesWithChildren)
-      );
-    const followUpAttributionsWithFormattedResources =
-      removeSlashesFromFilesWithChildren(
-        followUpAttributionsWithResources,
-        getFileWithChildrenCheck(filesWithChildren)
-      );
-
-    window.ipcRenderer.invoke(IpcChannel.ExportFile, {
-      type: ExportType.FollowUp,
-      followUpAttributionsWithResources:
-        followUpAttributionsWithFormattedResources,
-    });
-  }
-
-  function getSpdxDocumentExportListener(
-    exportType: ExportType.SpdxDocumentYaml | ExportType.SpdxDocumentJson
-  ): void {
-    const attributions = Object.fromEntries(
-      Object.entries(manualData.attributions).map((entry) => {
-        const packageInfo = entry[1];
-
-        const licenseName = packageInfo.licenseName || '';
-        const isFrequentLicense =
-          licenseName && licenseName in frequentLicenseTexts;
-        const licenseText =
-          packageInfo.licenseText || isFrequentLicense
-            ? frequentLicenseTexts[licenseName]
-            : '';
-        return [
-          entry[0],
-          {
-            ...entry[1],
-            licenseText,
-          },
-        ];
-      })
-    );
-
-    const args: ExportSpdxDocumentYamlArgs | ExportSpdxDocumentJsonArgs = {
-      type: exportType,
-      spdxAttributions: attributions,
-    };
-
-    window.ipcRenderer.invoke(IpcChannel.ExportFile, args);
-  }
-
-  function getDetailedBomExportListener(): void {
-    const bomAttributions = getBomAttributions(manualData.attributions);
-
-    const bomAttributionsWithResources = getAttributionsWithResources(
-      bomAttributions,
-      manualData.attributionsToResources
-    );
-
-    const bomAttributionsWithFormattedResources =
-      removeSlashesFromFilesWithChildren(
-        bomAttributionsWithResources,
-        getFileWithChildrenCheck(filesWithChildren)
-      );
-
-    window.ipcRenderer.invoke(IpcChannel.ExportFile, {
-      type: ExportType.DetailedBom,
-      bomAttributionsWithResources: bomAttributionsWithFormattedResources,
-    });
-  }
-
-  function getCompactBomExportListener(): void {
-    window.ipcRenderer.invoke(IpcChannel.ExportFile, {
-      type: ExportType.CompactBom,
-      bomAttributions: getBomAttributions(manualData.attributions),
-    });
-  }
-
-  function getBomAttributions(attributions: Attributions): Attributions {
-    return pick(
-      attributions,
-      Object.keys(attributions).filter(
-        (attributionId) =>
-          !attributions[attributionId].followUp &&
-          !attributions[attributionId].firstParty
-      )
-    );
-  }
-
-  function resetLoadedFileListener(
-    event: IpcRendererEvent,
-    resetState: boolean
-  ): void {
-    if (resetState) {
-      dispatch(resetResourceState());
-    }
-  }
-
-  function loggingListener(event: IpcRendererEvent, logging: string): void {
-    if (logging) {
-      console.log(logging);
-    }
-  }
-
-  function showSearchPopupListener(
-    event: IpcRendererEvent,
-    showSearchPopUp: boolean
-  ): void {
-    if (showSearchPopUp) {
-      dispatch(openPopup(PopupType.FileSearchPopup));
-    }
-  }
-
-  function showProjectMetadataPopupListener(
-    event: IpcRendererEvent,
-    showProjectMetadataPopup: boolean
-  ): void {
-    if (showProjectMetadataPopup) {
-      dispatch(openPopup(PopupType.ProjectMetadataPopup));
-    }
-  }
-
-  function setBaseURLForRootListener(
-    event: IpcRendererEvent,
-    baseURLForRootArgs: BaseURLForRootArgs
-  ): void {
-    if (baseURLForRootArgs?.baseURLForRoot) {
-      dispatch(
-        setBaseUrlsForSources({
-          ...baseUrlsForSources,
-          '/': baseURLForRootArgs.baseURLForRoot,
-        })
-      );
-    }
-  }
-
-  function setToggleHighlightForCriticalSignalsListener(
-    event: IpcRendererEvent,
-    toggleHighlightForCriticalSignalsArgs: ToggleHighlightForCriticalSignalsArgs
-  ): void {
-    if (
-      toggleHighlightForCriticalSignalsArgs?.toggleHighlightForCriticalSignals
-    ) {
-      dispatch(
-        setHighlightForCriticalSignals(!showHighlightForCriticalSignals)
-      );
-    }
-  }
-
-  useIpcRenderer(IpcChannel.FileLoaded, fileLoadedListener, [dispatch]);
-  useIpcRenderer(IpcChannel.ResetLoadedFile, resetLoadedFileListener, [
-    dispatch,
-  ]);
-  useIpcRenderer(IpcChannel.Logging, loggingListener, [dispatch]);
-  useIpcRenderer(IpcChannel.ShowSearchPopup, showSearchPopupListener, [
-    dispatch,
-  ]);
-  useIpcRenderer(
-    IpcChannel.ShowProjectMetadataPopup,
-    showProjectMetadataPopupListener,
-    [dispatch]
-  );
-  useIpcRenderer(IpcChannel.SetBaseURLForRoot, setBaseURLForRootListener, [
-    dispatch,
-    baseUrlsForSources,
-  ]);
-  useIpcRenderer(IpcChannel.ExportFileRequest, getExportFileRequestListener, [
-    manualData,
-    attributionBreakpoints,
-    frequentLicenseTexts,
-    filesWithChildren,
-  ]);
-  useIpcRenderer(
-    IpcChannel.ToggleHighlightForCriticalSignals,
-    setToggleHighlightForCriticalSignalsListener,
-    [dispatch, showHighlightForCriticalSignals]
-  );
 
   function handleClick(
     event: React.MouseEvent<HTMLElement>,
@@ -334,6 +73,7 @@ export function TopBar(): ReactElement {
 
   return (
     <div className={classes.root}>
+      <BackendCommunication />
       <IconButton
         tooltipTitle="open file"
         placement="right"
