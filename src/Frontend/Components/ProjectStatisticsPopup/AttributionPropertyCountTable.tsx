6f3b2f880d8942e741bcc41787fd6459fdcb5b19
someshkhandelia <someshkhandelia@gmail.com>
2022-09-20 17:23:49 +0200

    Improve styling
    - Make tables in statistics popup look more modular.
    
    Fix: #948
    
    Signed-off-by: someshkhandelia <someshkhandelia@gmail.com>

--- src/Frontend/Components/ProjectStatisticsPopup/AttributionPropertyCountTable.tsx
+++ src/Frontend/Components/ProjectStatisticsPopup/AttributionPropertyCountTable.tsx
@@ -7,12 +7,13 @@ import React, { ReactElement } from 'react';
 import MuiTypography from '@mui/material/Typography';
 import MuiBox from '@mui/material/Box';
 import MuiTable from '@mui/material/Table';
-import MuiTableBody from '@mui/material/TableBody';
 import MuiTableCell from '@mui/material/TableCell';
 import MuiTableContainer from '@mui/material/TableContainer';
 import MuiTableRow from '@mui/material/TableRow';
 import { getAttributionPropertyDisplayNameFromId } from './project-statistics-popup-helpers';
 import { projectStatisticsPopupClasses } from './shared-project-statistics-popup-styles';
+import MuiTableHead from '@mui/material/TableHead';
+import MuiTableFooter from '@mui/material/TableFooter';
 
 interface AttributionPropertyCountTableProps {
   attributionPropertyCountsEntries: Array<Array<string | number>>;
@@ -22,33 +23,51 @@ interface AttributionPropertyCountTableProps {
 export function AttributionPropertyCountTable(
   props: AttributionPropertyCountTableProps
 ): ReactElement {
+  const attributionPropertyDisplayNames =
+    props.attributionPropertyCountsEntries.map((entry) =>
+      getAttributionPropertyDisplayNameFromId(entry[0].toString())
+    );
+  const attributionPropertyCounts = props.attributionPropertyCountsEntries.map(
+    (entry) => entry[1].toString()
+  );
+
   return (
     <MuiBox>
       <MuiTypography variant="subtitle1">{props.title}</MuiTypography>
-      <MuiTableContainer sx={projectStatisticsPopupClasses.smallTable}>
-        <MuiTable size="small">
-          <MuiTableBody>
-            {props.attributionPropertyCountsEntries.map(
-              ([attributionProperty, count], index) => (
-                <MuiTableRow key={index}>
+      <MuiTableContainer
+        sx={projectStatisticsPopupClasses.attributionPropertyCountTable}
+      >
+        <MuiTable size="small" stickyHeader>
+          <MuiTableHead>
+            <MuiTableRow>
+              {attributionPropertyDisplayNames.map(
+                (attributionPropertyDisplayName, index) => (
                   <MuiTableCell
-                    sx={projectStatisticsPopupClasses.body}
+                    sx={projectStatisticsPopupClasses.head}
+                    key={index}
                     align="center"
                   >
-                    {getAttributionPropertyDisplayNameFromId(
-                      attributionProperty.toString()
-                    )}
+                    {attributionPropertyDisplayName}
                   </MuiTableCell>
+                )
+              )}
+            </MuiTableRow>
+          </MuiTableHead>
+          <MuiTableFooter sx={projectStatisticsPopupClasses.tableFooter}>
+            <MuiTableRow>
+              {attributionPropertyCounts.map(
+                (attributionPropertyCount, index) => (
                   <MuiTableCell
                     sx={projectStatisticsPopupClasses.body}
+                    key={index}
                     align="center"
                   >
-                    {count}
+                    {attributionPropertyCount}
                   </MuiTableCell>
-                </MuiTableRow>
-              )
-            )}
-          </MuiTableBody>
+                )
+              )}
+            </MuiTableRow>
+          </MuiTableFooter>
         </MuiTable>
       </MuiTableContainer>
     </MuiBox>
