opossum-tool/OpossumUI

commit 94f7c52eedc30244c78219e41bb22b50a90d8fda

Add a search to the attribution column in the audit view.
Add a search functionality for signals. This is accessed by clicking on the search icon, which is located next to the tabs. This opens a textbox that allows a user to search attributions by package name, license name and copyright.

Signed-off-by: Markus Obendrauf <markus.obendrauf@tngtech.com>

src/Frontend/Components/PackageList/AttributionsViewPackageList.tsx
@@ -1,0 +1,54 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { ReactElement, useMemo, useState } from 'react';
+import { Attributions } from '../../../shared/shared-types';
+import { Height, NumberOfDisplayedItems } from '../../types/types';
+import { List } from '../List/List';
+import { SearchTextField } from '../SearchTextField/SearchTextField';
+import { getSortedFilteredPackageIds } from './package-list-helpers';
+
+const CARD_VERTICAL_DISTANCE = 41;
+
+interface AttributionsViewPackageListProps {
+  attributions: Attributions;
+  attributionIds: Array<string>;
+  getAttributionCard(attributionId: string): ReactElement | null;
+  max: NumberOfDisplayedItems | Height;
+}
+
+export function AttributionsViewPackageList(
+  props: AttributionsViewPackageListProps
+): ReactElement {
+  const [search, setSearch] = useState('');
+
+  const sortedFilteredPackageIds: Array<string> = useMemo(
+    () =>
+      getSortedFilteredPackageIds(
+        props.attributions,
+        props.attributionIds,
+        search
+      ),
+    [props.attributions, props.attributionIds, search]
+  );
+
+  return (
+    <div>
+      <SearchTextField
+        onInputChange={setSearch}
+        search={search}
+        showIcon={true}
+      />
+      <List
+        getListItem={(index: number): ReactElement | null =>
+          props.getAttributionCard(sortedFilteredPackageIds[index])
+        }
+        max={props.max}
+        length={sortedFilteredPackageIds.length}
+        cardVerticalDistance={CARD_VERTICAL_DISTANCE}
+      />
+    </div>
+  );
+}

src/Frontend/Components/PackageList/PackageList.tsx
@@ -3,20 +3,15 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { ReactElement } from 'react';
-import {
-  AttributionIdWithCount,
-  Attributions,
-  PackageInfo,
-} from '../../../shared/shared-types';
-import { getAlphabeticalComparer } from '../../util/get-alphabetical-comparer';
+import React, { ReactElement, useMemo } from 'react';
+import { Attributions } from '../../../shared/shared-types';
+import MuiTypography from '@mui/material/Typography';
 import { List } from '../List/List';
-import { PackagePanelCard } from '../PackagePanelCard/PackagePanelCard';
-import { ListCardConfig } from '../../types/types';
+import { useAppSelector } from '../../state/hooks';
+import { getPackageSearchTerm } from '../../state/selectors/audit-view-resource-selectors';
+import { getSortedFilteredPackageIds } from './package-list-helpers';
 
-const NUMBER_OF_DISPLAYED_ITEMS = 15;
 const CARD_VERTICAL_DISTANCE = 41;
-const MAX_HEIGHT = NUMBER_OF_DISPLAYED_ITEMS * CARD_VERTICAL_DISTANCE;
 const TYPICAL_SCROLLBAR_WIDTH = 13;
 
 const classes = {
@@ -26,102 +21,47 @@ const classes = {
 };
 
 interface PackageListProps {
-  attributionIdsWithCount?: Array<AttributionIdWithCount>;
   attributions: Attributions;
-  selectedAttributionId: string | null;
-  resolvedAttributionIds: Set<string>;
-  onCardClick(attributionId: string): void;
-  onAddAttributionClick(attributionId: string): void;
-  sorted?: boolean;
-  isExternalAttribution: boolean;
-  preSelectedExternalAttributionIdsForSelectedResource: Array<string>;
-  isAddToPackageEnabled: boolean;
-  selectedResourceId: string;
+  attributionIds: Array<string>;
+  getAttributionCard(attributionId: string): ReactElement | null;
+  maxNumberOfDisplayedItems: number;
+  listTitle: string;
 }
 
 export function PackageList(props: PackageListProps): ReactElement {
-  const attributionIdsWithCount = props.attributionIdsWithCount || [];
+  const searchTerm = useAppSelector(getPackageSearchTerm);
 
-  function getSortedAttributionIds(): Array<string> {
-    if (!attributionIdsWithCount.length) {
-      return [];
-    }
+  const sortedFilteredPackageIds: Array<string> = useMemo(
+    () =>
+      getSortedFilteredPackageIds(
+        props.attributions,
+        props.attributionIds,
+        searchTerm
+      ),
+    [props.attributions, props.attributionIds, searchTerm]
+  );
 
-    const attributionIds = attributionIdsWithCount.map(
-      (attributionIdWithCount) => attributionIdWithCount.attributionId
-    );
-
-    return props.sorted
-      ? attributionIds.sort(getAlphabeticalComparer(props.attributions))
-      : attributionIds;
-  }
-
-  const sortedAttributionIds = getSortedAttributionIds();
-
-  function getPackagePanelCard(index: number): ReactElement {
-    const attributionId: string = sortedAttributionIds[index];
-    const packageInfo: PackageInfo = props.attributions[attributionId];
-    const packageCount = attributionIdsWithCount.filter(
-      (attributionIdWithCount) =>
-        attributionIdWithCount.attributionId === attributionId
-    )[0].childrenWithAttributionCount;
-    const isPreselected = props.isExternalAttribution
-      ? props.preSelectedExternalAttributionIdsForSelectedResource.includes(
-          attributionId
-        )
-      : packageInfo.preSelected;
-
-    const cardConfig: ListCardConfig = {
-      isSelected: attributionId === props.selectedAttributionId,
-      isPreSelected: isPreselected,
-      isResolved: props.resolvedAttributionIds.has(attributionId),
-      isExternalAttribution: props.isExternalAttribution,
-      firstParty: packageInfo.firstParty,
-      excludeFromNotice: packageInfo.excludeFromNotice,
-      followUp: Boolean(packageInfo.followUp),
-      criticality: props.isExternalAttribution
-        ? packageInfo.criticality
-        : packageInfo.preSelected
-        ? packageInfo.criticality
-        : undefined,
-    };
-
-    function onIconClick(): void {
-      props.onAddAttributionClick(attributionId);
-    }
-
-    return (
-      <PackagePanelCard
-        onClick={(): void => props.onCardClick(attributionId)}
-        onIconClick={props.isAddToPackageEnabled ? onIconClick : undefined}
-        key={`PackageCard-${packageInfo.packageName}-${index}`}
-        packageCount={packageCount}
-        hideResourceSpecificButtons={true}
-        cardContent={{
-          id: `package-${props.selectedResourceId}-${attributionId}`,
-          name: packageInfo.packageName,
-          packageVersion: packageInfo.packageVersion,
-          copyright: packageInfo.copyright,
-          licenseText: packageInfo.licenseText,
-          comment: packageInfo.comment,
-          url: packageInfo.url,
-          licenseName: packageInfo.licenseName,
-        }}
-        attributionId={attributionId}
-        cardConfig={cardConfig}
-      />
-    );
-  }
-
-  const currentHeight = attributionIdsWithCount.length * CARD_VERTICAL_DISTANCE;
+  const currentHeight = props.attributionIds.length * CARD_VERTICAL_DISTANCE;
+  const maxHeight = props.maxNumberOfDisplayedItems * CARD_VERTICAL_DISTANCE;
 
   return (
-    <List
-      getListItem={getPackagePanelCard}
-      max={{ numberOfDisplayedItems: NUMBER_OF_DISPLAYED_ITEMS }}
-      length={attributionIdsWithCount.length}
-      cardVerticalDistance={CARD_VERTICAL_DISTANCE}
-      sx={currentHeight < MAX_HEIGHT ? classes.paddingRight : {}}
-    />
+    <>
+      {sortedFilteredPackageIds.length === 0 ? null : (
+        <>
+          {props.listTitle ? (
+            <MuiTypography variant={'body2'}>{props.listTitle}</MuiTypography>
+          ) : null}
+          <List
+            getListItem={(index: number): ReactElement | null =>
+              props.getAttributionCard(sortedFilteredPackageIds[index])
+            }
+            max={{ numberOfDisplayedItems: props.maxNumberOfDisplayedItems }}
+            length={sortedFilteredPackageIds.length}
+            cardVerticalDistance={CARD_VERTICAL_DISTANCE}
+            sx={currentHeight < maxHeight ? classes.paddingRight : {}}
+          />
+        </>
+      )}
+    </>
   );
 }

src/Frontend/Components/PackageList/package-list-helpers.ts
@@ -1,0 +1,43 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { Attributions, PackageInfo } from '../../../shared/shared-types';
+import { getAlphabeticalComparer } from '../../util/get-alphabetical-comparer';
+
+function attributionContainsSearchTerm(
+  attribution: PackageInfo,
+  searchTerm: string
+): boolean {
+  return Boolean(
+    attribution &&
+      (searchTerm === '' ||
+        (attribution.packageName &&
+          attribution.packageName
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())) ||
+        (attribution.licenseName &&
+          attribution.licenseName
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())) ||
+        (attribution.copyright &&
+          attribution.copyright
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())) ||
+        (attribution.packageVersion &&
+          attribution.packageVersion
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())))
+  );
+}
+
+export function getSortedFilteredPackageIds(
+  attributions: Attributions,
+  attributionIds: Array<string>,
+  searchTerm: string
+): Array<string> {
+  return attributionIds
+    .filter((id) => attributionContainsSearchTerm(attributions[id], searchTerm))
+    .sort(getAlphabeticalComparer(attributions));
+}

src/Frontend/Components/PackageList/__tests__/package-list-helpers.test.ts
@@ -1,0 +1,68 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { Attributions } from '../../../../shared/shared-types';
+import { getSortedFilteredPackageIds } from '../package-list-helpers';
+
+describe('The PackageListHelper', () => {
+  it('filters Attributions', () => {
+    const testAttributions: Attributions = {
+      uuid1: {
+        packageName: 'Search_term package',
+      },
+      uuid2: {
+        copyright: '(c) Search_term 2022',
+      },
+      uuid3: {
+        licenseName: 'Search_term licence',
+      },
+      uuid4: {
+        packageVersion: 'version search_term',
+      },
+      uuid5: {
+        comment: 'comment search_term',
+        licenseText: 'text search_term',
+        url: 'www.search_term.com',
+      },
+    };
+    const testAttributionIds = Object.entries(testAttributions).map(
+      ([attributionId]) => attributionId
+    );
+    const sortedFilteredTestAttributions = getSortedFilteredPackageIds(
+      testAttributions,
+      testAttributionIds,
+      'SeArCh_TeRm'
+    );
+
+    expect(sortedFilteredTestAttributions).toContain('uuid1');
+    expect(sortedFilteredTestAttributions).toContain('uuid2');
+    expect(sortedFilteredTestAttributions).toContain('uuid3');
+    expect(sortedFilteredTestAttributions).toContain('uuid4');
+    expect(sortedFilteredTestAttributions).not.toContain('uuid5');
+  });
+
+  it('sorts Attributions', () => {
+    const testAttributions = {
+      uuid1: {
+        packageName: 'package 3',
+      },
+      uuid2: {
+        packageName: 'package 1',
+      },
+      uuid3: {
+        packageName: 'package 2',
+      },
+    };
+    const testAttributionIds = Object.entries(testAttributions).map(
+      ([attributionId]) => attributionId
+    );
+    const sortedFilteredTestAttributions = getSortedFilteredPackageIds(
+      testAttributions,
+      testAttributionIds,
+      ''
+    );
+    expect(sortedFilteredTestAttributions).toEqual(['uuid2', 'uuid3', 'uuid1']);
+  });
+});

src/Frontend/Components/AllAttributionsPanel/AllAttributionsPanel.tsx
@@ -4,17 +4,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import MuiPaper from '@mui/material/Paper';
-import MuiTypography from '@mui/material/Typography';
 import React, { ReactElement } from 'react';
 import { Attributions } from '../../../shared/shared-types';
 import { PackagePanelTitle } from '../../enums/enums';
 import { selectAttributionInAccordionPanelOrOpenUnsavedPopup } from '../../state/actions/popup-actions/popup-actions';
 import { addToSelectedResource } from '../../state/actions/resource-actions/save-actions';
-import { FilteredList } from '../FilteredList/FilteredList';
 import { PackagePanelCard } from '../PackagePanelCard/PackagePanelCard';
 import { OpossumColors } from '../../shared-styles';
 import { ListCardConfig } from '../../types/types';
 import { useAppDispatch } from '../../state/hooks';
+import { PackageList } from '../PackageList/PackageList';
 
 const classes = {
   root: {
@@ -84,13 +83,12 @@ export function AllAttributionsPanel(
 
   return (
     <MuiPaper sx={classes.root} elevation={0} square={true}>
-      <MuiTypography>{PackagePanelTitle.AllAttributions}</MuiTypography>
-      <FilteredList
+      <PackageList
         attributions={props.attributions}
         attributionIds={props.attributionIds}
         getAttributionCard={getPackagePanelCard}
-        max={{ numberOfDisplayedItems: 20 }}
-        cardVerticalDistance={41}
+        maxNumberOfDisplayedItems={20}
+        listTitle={PackagePanelTitle.AllAttributions}
       />
     </MuiPaper>
   );

src/Frontend/Components/AllAttributionsPanel/__tests__/AllAttributionsPanel.test.tsx
@@ -3,7 +3,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import {
   Attributions,
@@ -111,47 +111,6 @@ describe('The AllAttributionsPanel', () => {
     expect(screen.queryByText('Typescript, 1.0')).not.toBeInTheDocument();
     expect(screen.getByText('React, 2.0')).toBeInTheDocument();
     expect(screen.getByText('Vue, 3.0')).toBeInTheDocument();
-  });
-
-  it('has search functionality', () => {
-    const testAttributions: Attributions = {
-      uuid1: {
-        packageName: 'name 1',
-        licenseText: 'text',
-        licenseName: 'license name 2',
-      },
-      uuid2: { packageName: 'name 2', copyright: '(c)' },
-    };
-    const testStore = createTestAppStore();
-    testStore.dispatch(
-      loadFromFile(
-        getParsedInputFileEnrichedWithTestData({
-          manualAttributions: testAttributions,
-        })
-      )
-    );
-    renderComponentWithStore(
-      <AllAttributionsPanel
-        attributions={testAttributions}
-        selectedAttributionId={null}
-        attributionIds={['uuid1', 'uuid2']}
-        isAddToPackageEnabled={true}
-      />,
-      { store: testStore }
-    );
-    screen.getByText('name 1');
-    screen.getByText('name 2');
-
-    fireEvent.change(screen.getByRole('searchbox'), {
-      target: { value: 'name 1' },
-    });
-    screen.getByText('name 1');
-
-    fireEvent.change(screen.getByRole('searchbox'), {
-      target: { value: 'name' },
-    });
-    screen.getByText('name 1');
-    screen.getByText('name 2');
   });
 
   it('shows correct replace attribution buttons in the context menu', () => {

src/Frontend/Components/AttributionColumn/PackageSubPanel.tsx
@@ -59,7 +59,7 @@ export function PackageSubPanel(props: PackageSubPanelProps): ReactElement {
           endIcon={
             <IconButton
               tooltipTitle="Search for package information"
-              placement="right"
+              tooltipPlacement="right"
               onClick={props.openPackageSearchPopup}
               disabled={!props.isEditable}
               icon={
@@ -135,7 +135,7 @@ export function PackageSubPanel(props: PackageSubPanelProps): ReactElement {
             />
             <IconButton
               tooltipTitle="open link in browser"
-              placement="right"
+              tooltipPlacement="right"
               onClick={openUrl}
               disabled={!props.displayPackageInfo.url}
               icon={

src/Frontend/Components/AttributionList/AttributionList.tsx
@@ -7,12 +7,12 @@ import MuiTypography from '@mui/material/Typography';
 import React, { ReactElement } from 'react';
 import { Attributions } from '../../../shared/shared-types';
 import { getAlphabeticalComparer } from '../../util/get-alphabetical-comparer';
-import { FilteredList } from '../FilteredList/FilteredList';
 import { PackageCard } from '../PackageCard/PackageCard';
 import { ListCardConfig } from '../../types/types';
 import { checkboxClass } from '../../shared-styles';
 import MuiBox from '@mui/material/Box';
 import { SxProps } from '@mui/material';
+import { AttributionsViewPackageList } from '../PackageList/AttributionsViewPackageList';
 
 const classes = {
   ...checkboxClass,
@@ -101,12 +101,11 @@ export function AttributionList(props: AttributionListProps): ReactElement {
         {props.topRightElement}
       </MuiBox>
       {props.filterElement}
-      <FilteredList
+      <AttributionsViewPackageList
         attributions={props.attributions}
         attributionIds={attributionIds}
         getAttributionCard={getAttributionCard}
         max={{ height: props.maxHeight }}
-        cardVerticalDistance={41}
       />
     </MuiBox>
   );

commit fea602543b6f643827c5b335aa1f4146ccf95aef

[Bug-Fix] Fix attributions sorting order
- Attributions need not be re-sorted alphabetically since we are already getting the correctly sorted attributions.
- The correct sorting order of attributions is in descending order of number of attributions.
- We also renamed the function and certain associated variables, now that sorting is not part of the function anymore.
- Bug was introduced in the commit: https://github.com/opossum-tool/OpossumUI/commit/94f7c52eedc30244c78219e41bb22b50a90d8fda

Fix: #989

Signed-off-by: someshkhandelia <someshkhandelia@gmail.com>

src/Frontend/Components/PackageList/AttributionsViewPackageList.tsx
@@ -8,14 +8,16 @@ import { Attributions } from '../../../shared/shared-types';
 import { Height, NumberOfDisplayedItems } from '../../types/types';
 import { List } from '../List/List';
 import { SearchTextField } from '../SearchTextField/SearchTextField';
-import { getSortedFilteredPackageIds } from './package-list-helpers';
+import { getFilteredPackageIds } from './package-list-helpers';
 
 const CARD_VERTICAL_DISTANCE = 41;
 
 interface AttributionsViewPackageListProps {
   attributions: Attributions;
   attributionIds: Array<string>;
+
   getAttributionCard(attributionId: string): ReactElement | null;
+
   max: NumberOfDisplayedItems | Height;
 }
 
@@ -24,13 +26,9 @@ export function AttributionsViewPackageList(
 ): ReactElement {
   const [search, setSearch] = useState('');
 
-  const sortedFilteredPackageIds: Array<string> = useMemo(
+  const filteredPackageIds: Array<string> = useMemo(
     () =>
-      getSortedFilteredPackageIds(
-        props.attributions,
-        props.attributionIds,
-        search
-      ),
+      getFilteredPackageIds(props.attributions, props.attributionIds, search),
     [props.attributions, props.attributionIds, search]
   );
 
@@ -43,10 +41,10 @@ export function AttributionsViewPackageList(
       />
       <List
         getListItem={(index: number): ReactElement | null =>
-          props.getAttributionCard(sortedFilteredPackageIds[index])
+          props.getAttributionCard(filteredPackageIds[index])
         }
         max={props.max}
-        length={sortedFilteredPackageIds.length}
+        length={filteredPackageIds.length}
         cardVerticalDistance={CARD_VERTICAL_DISTANCE}
       />
     </div>

src/Frontend/Components/PackageList/PackageList.tsx
@@ -9,7 +9,7 @@ import MuiTypography from '@mui/material/Typography';
 import { List } from '../List/List';
 import { useAppSelector } from '../../state/hooks';
 import { getPackageSearchTerm } from '../../state/selectors/audit-view-resource-selectors';
-import { getSortedFilteredPackageIds } from './package-list-helpers';
+import { getFilteredPackageIds } from './package-list-helpers';
 
 const CARD_VERTICAL_DISTANCE = 41;
 const TYPICAL_SCROLLBAR_WIDTH = 13;
@@ -23,7 +23,9 @@ const classes = {
 interface PackageListProps {
   attributions: Attributions;
   attributionIds: Array<string>;
+
   getAttributionCard(attributionId: string): ReactElement | null;
+
   maxNumberOfDisplayedItems: number;
   listTitle: string;
 }
@@ -31,9 +33,9 @@ interface PackageListProps {
 export function PackageList(props: PackageListProps): ReactElement {
   const searchTerm = useAppSelector(getPackageSearchTerm);
 
-  const sortedFilteredPackageIds: Array<string> = useMemo(
+  const filteredPackageIds: Array<string> = useMemo(
     () =>
-      getSortedFilteredPackageIds(
+      getFilteredPackageIds(
         props.attributions,
         props.attributionIds,
         searchTerm
@@ -46,17 +48,17 @@ export function PackageList(props: PackageListProps): ReactElement {
 
   return (
     <>
-      {sortedFilteredPackageIds.length === 0 ? null : (
+      {filteredPackageIds.length === 0 ? null : (
         <>
           {props.listTitle ? (
             <MuiTypography variant={'body2'}>{props.listTitle}</MuiTypography>
           ) : null}
           <List
             getListItem={(index: number): ReactElement | null =>
-              props.getAttributionCard(sortedFilteredPackageIds[index])
+              props.getAttributionCard(filteredPackageIds[index])
             }
             max={{ numberOfDisplayedItems: props.maxNumberOfDisplayedItems }}
-            length={sortedFilteredPackageIds.length}
+            length={filteredPackageIds.length}
             cardVerticalDistance={CARD_VERTICAL_DISTANCE}
             sx={currentHeight < maxHeight ? classes.paddingRight : {}}
           />

src/Frontend/Components/PackageList/package-list-helpers.ts
@@ -4,16 +4,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Attributions, PackageInfo } from '../../../shared/shared-types';
-import { getAlphabeticalComparer } from '../../util/get-alphabetical-comparer';
 
-export function getSortedFilteredPackageIds(
+export function getFilteredPackageIds(
   attributions: Attributions,
   attributionIds: Array<string>,
   searchTerm: string
 ): Array<string> {
-  return attributionIds
-    .filter((id) => attributionContainsSearchTerm(attributions[id], searchTerm))
-    .sort(getAlphabeticalComparer(attributions));
+  return attributionIds.filter((id) =>
+    attributionContainsSearchTerm(attributions[id], searchTerm)
+  );
 }
 
 function attributionContainsSearchTerm(

src/Frontend/Components/PackageList/__tests__/package-list-helpers.test.ts
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Attributions } from '../../../../shared/shared-types';
-import { getSortedFilteredPackageIds } from '../package-list-helpers';
+import { getFilteredPackageIds } from '../package-list-helpers';
 
 describe('The PackageListHelper', () => {
   it('filters Attributions', () => {
@@ -30,17 +30,17 @@ describe('The PackageListHelper', () => {
     const testAttributionIds = Object.entries(testAttributions).map(
       ([attributionId]) => attributionId
     );
-    const sortedFilteredTestAttributions = getSortedFilteredPackageIds(
+    const filteredTestAttributions = getFilteredPackageIds(
       testAttributions,
       testAttributionIds,
       'SeArCh_TeRm'
     );
 
-    expect(sortedFilteredTestAttributions).toContain('uuid1');
-    expect(sortedFilteredTestAttributions).toContain('uuid2');
-    expect(sortedFilteredTestAttributions).toContain('uuid3');
-    expect(sortedFilteredTestAttributions).toContain('uuid4');
-    expect(sortedFilteredTestAttributions).not.toContain('uuid5');
+    expect(filteredTestAttributions).toContain('uuid1');
+    expect(filteredTestAttributions).toContain('uuid2');
+    expect(filteredTestAttributions).toContain('uuid3');
+    expect(filteredTestAttributions).toContain('uuid4');
+    expect(filteredTestAttributions).not.toContain('uuid5');
   });
 
   it('sorts Attributions', () => {
@@ -58,11 +58,11 @@ describe('The PackageListHelper', () => {
     const testAttributionIds = Object.entries(testAttributions).map(
       ([attributionId]) => attributionId
     );
-    const sortedFilteredTestAttributions = getSortedFilteredPackageIds(
+    const filteredTestAttributions = getFilteredPackageIds(
       testAttributions,
       testAttributionIds,
       ''
     );
-    expect(sortedFilteredTestAttributions).toEqual(['uuid2', 'uuid3', 'uuid1']);
+    expect(filteredTestAttributions).toEqual(['uuid1', 'uuid2', 'uuid3']);
   });
 });