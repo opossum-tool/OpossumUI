96e128e93efcf64cffc0d76287dc2afb212b1a85
Lennart Holstein <lennart.holstein@tngtech.com>
2022-11-17 10:20:56 +0100

    Improved styling of the project metadata popup table
    Use colors from the project statistics popup table. Width of project
    metadata popup is now flexible and adapts to the table content.
    
    Fix: #1010
    
    Signed-off-by: Lennart Holstein <lennart.holstein@tngtech.com>

--- src/Frontend/Components/ProjectMetadataPopup/ProjectMetadataPopup.tsx
+++ src/Frontend/Components/ProjectMetadataPopup/ProjectMetadataPopup.tsx
@@ -5,79 +5,24 @@
 
 import React, { ReactElement } from 'react';
 import { NotificationPopup } from '../NotificationPopup/NotificationPopup';
-import { useAppDispatch, useAppSelector } from '../../state/hooks';
+import { useAppDispatch } from '../../state/hooks';
 import { closePopup } from '../../state/actions/view-actions/view-actions';
-import MuiTable from '@mui/material/Table';
-import MuiTableBody from '@mui/material/TableBody';
-import MuiTableCell from '@mui/material/TableCell';
-import MuiTableContainer from '@mui/material/TableContainer';
-import MuiTableRow from '@mui/material/TableRow';
-import MuiPaper from '@mui/material/Paper';
-import { getProjectMetadata } from '../../state/selectors/all-views-resource-selectors';
-import { ProjectMetadata } from '../../../shared/shared-types';
 import { ButtonText } from '../../enums/enums';
-
-const classes = {
-  table: {
-    minWidth: '350px',
-  },
-};
+import { ProjectMetadataTable } from '../ProjectMetadataTable/ProjectMetadataTable';
 
 export function ProjectMetadataPopup(): ReactElement {
   const dispatch = useAppDispatch();
-  const metadata: ProjectMetadata = useAppSelector(getProjectMetadata);
 
   function close(): void {
     dispatch(closePopup());
   }
 
-  function mapKeyToString(key: string): string {
-    const keyToDisplayName: { [key: string]: string } = {
-      projectTitle: 'Project Title',
-      projectId: 'Project ID',
-      fileCreationDate: 'File Creation Date',
-    };
-
-    if (key in keyToDisplayName) {
-      return keyToDisplayName[key];
-    } else {
-      return key;
-    }
-  }
-
-  function getMuiTableRow(key: string): ReactElement {
-    return (
-      <MuiTableRow key={key}>
-        <MuiTableCell component="th" scope="row">
-          {mapKeyToString(key)}
-        </MuiTableCell>
-        <MuiTableCell>
-          <pre>
-            {typeof metadata[key] === 'string'
-              ? (metadata[key] as string)
-              : JSON.stringify(metadata[key], null, 2)}
-          </pre>
-        </MuiTableCell>
-      </MuiTableRow>
-    );
-  }
-
-  const content = (
-    <MuiTableContainer component={MuiPaper}>
-      <MuiTable sx={classes.table} aria-label="project metadata table">
-        <MuiTableBody>
-          {Object.keys(metadata).map((key) => getMuiTableRow(key))}
-        </MuiTableBody>
-      </MuiTable>
-    </MuiTableContainer>
-  );
-
   return (
     <NotificationPopup
-      content={content}
+      content={<ProjectMetadataTable />}
       header={'Project Metadata'}
       isOpen={true}
-      fullWidth={true}
+      fullWidth={false}
       rightButtonConfig={{
         onClick: close,
         buttonText: ButtonText.Close,
---
+++ src/Frontend/Components/ProjectMetadataTable/ProjectMetadataTable.tsx
@@ -0,0 +1,85 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { ReactElement } from 'react';
+import MuiBox from '@mui/material/Box';
+import MuiTable from '@mui/material/Table';
+import MuiTableBody from '@mui/material/TableBody';
+import MuiTableCell from '@mui/material/TableCell';
+import MuiTableContainer from '@mui/material/TableContainer';
+import MuiTableRow from '@mui/material/TableRow';
+import { getProjectMetadata } from '../../state/selectors/all-views-resource-selectors';
+import { useAppSelector } from '../../state/hooks';
+import MuiTypography from '@mui/material/Typography';
+
+import { OpossumColors } from '../../shared-styles';
+
+export const projectMetadataPopupClasses = {
+  firstColumn: {
+    fontSize: 13,
+    background: OpossumColors.darkBlue,
+    color: OpossumColors.white,
+    width: 'max-content',
+  },
+  secondColumn: {
+    fontSize: 11,
+    background: OpossumColors.lightestBlue,
+    width: 'max-content',
+    overflow: 'auto',
+    color: OpossumColors.black,
+  },
+  projectMetadataTableContainer: {
+    maxHeight: '500px',
+    width: 'max-content',
+    marginBottom: 3,
+  },
+};
+
+const keyToDisplayName: { [key: string]: string } = {
+  projectTitle: 'Project Title',
+  projectId: 'Project ID',
+  fileCreationDate: 'File Creation Date',
+};
+
+export function ProjectMetadataTable(): ReactElement {
+  const projectMetadata = useAppSelector(getProjectMetadata);
+
+  function mapKeyToDisplayName(key: string): string {
+    return key in keyToDisplayName ? keyToDisplayName[key] : key;
+  }
+
+  function getTableRow(key: string): ReactElement {
+    return (
+      <MuiTableRow key={key}>
+        <MuiTableCell sx={projectMetadataPopupClasses.firstColumn}>
+          {mapKeyToDisplayName(key)}
+        </MuiTableCell>
+        <MuiTableCell sx={projectMetadataPopupClasses.secondColumn}>
+          <pre>
+            <MuiTypography fontFamily="default">
+              {typeof projectMetadata[key] === 'string'
+                ? (projectMetadata[key] as string)
+                : JSON.stringify(projectMetadata[key], null, 2)}
+            </MuiTypography>
+          </pre>
+        </MuiTableCell>
+      </MuiTableRow>
+    );
+  }
+
+  return (
+    <MuiBox>
+      <MuiTableContainer
+        sx={projectMetadataPopupClasses.projectMetadataTableContainer}
+      >
+        <MuiTable>
+          <MuiTableBody>
+            {Object.keys(projectMetadata).map((key) => getTableRow(key))}
+          </MuiTableBody>
+        </MuiTable>
+      </MuiTableContainer>
+    </MuiBox>
+  );
+}
