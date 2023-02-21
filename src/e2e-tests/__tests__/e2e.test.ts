1899dada94a82bdbbb1cd09acea3376fce0563ec
Markus Obendrauf <markus.obendrauf@tngtech.com>
2023-02-21 11:33:47 +0100

    Write E2E tests for .opossum file format
    Issue: #1161
    
    Signed-off-by: Markus Obendrauf <markus.obendrauf@tngtech.com>

--- src/e2e-tests/__tests__/e2e.test.ts
+++ src/e2e-tests/__tests__/e2e.test.ts
@@ -45,7 +45,60 @@ test.describe('The OpossumUI', () => {
   });
 });
 
-test.describe('Open file via command line', () => {
+test.describe('Open .opossum file via command line', () => {
+  let app: ElectronApplication;
+  let window: Page;
+
+  test.beforeEach(async () => {
+    app = await getApp(
+      'src/e2e-tests/test-resources/opossum_input_and_output_e2e.opossum'
+    );
+    window = await app.firstWindow();
+    await window.waitForLoadState();
+  });
+
+  test.afterEach(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
+  test('should open file when provided as command line arg', async () => {
+    await getElementWithText(window, 'Frontend');
+
+    const electronBackendEntry = await getElementWithText(
+      window,
+      'ElectronBackend'
+    );
+    await electronBackendEntry.click();
+  });
+
+  test('should show signals and attributions in accordions', async () => {
+    const electronBackendEntry = await getElementWithText(
+      window,
+      'ElectronBackend'
+    );
+    await electronBackendEntry.click();
+
+    await expect(window.locator(`text=${'jQuery, 16.13.1'}`)).toBeVisible({
+      timeout: EXPECT_TIMEOUT,
+    });
+
+    await expect(window.locator(`text=${'Apache'}`)).toHaveCount(1, {
+      timeout: EXPECT_TIMEOUT,
+    });
+
+    const signalsInFolderContentEntry = await getElementWithText(
+      window,
+      'Signals in Folder Content'
+    );
+    await signalsInFolderContentEntry.click();
+
+    await expect(window.locator(`text=${'jQuery, 16.13.1'}`)).toBeHidden();
+  });
+});
+
+test.describe('Open .json file via command line', () => {
   let app: ElectronApplication;
   let window: Page;
 
