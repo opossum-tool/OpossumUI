8912ef3d3cb0b9c4543785f35c4d2af4a77709d3
Vasily Pozdnyakov <vasily.pozdnyakov@tngtech.com>
2023-05-03 13:15:55 +0200

    Added NeedsReview filter
    Signed-off-by: Vasily Pozdnyakov <vasily.pozdnyakov@tngtech.com>

--- src/Frontend/enums/enums.ts
+++ src/Frontend/enums/enums.ts
@@ -78,6 +78,7 @@ export enum FilterType {
   OnlyFirstParty = 'Only First Party',
   HideFirstParty = 'Hide First Party',
   OnlyFollowUp = 'Only Follow Up',
+  OnlyNeedsReview = 'Only Needs Review',
 }
 
 export enum CheckboxLabel {
