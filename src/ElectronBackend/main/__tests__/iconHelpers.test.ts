fe8df3b236d07c1c478b15141892f4ac4a15392d
Meret Behrens <meret.behrens@tngtech.com>
2023-10-04 11:09:19 +0200

    Add menu item for QA mode
    This commit adds a new entry to the menu to select the new QA mode. If the mode is selected the icon will be a checked checkbox if not an unchecked one.
    
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ src/ElectronBackend/main/__tests__/iconHelpers.test.ts
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { Menu, MenuItem } from 'electron';
+import { makeFirstIconVisibleAndSecondHidden } from '../iconHelpers';
+
+jest.mock('electron', () => ({
+  Menu: {
+    setApplicationMenu: jest.fn(),
+    buildFromTemplate: jest.fn(),
+    getApplicationMenu: jest.fn(),
+  },
+}));
+jest.mock('electron-is-dev', () => false);
+
+describe('makeFirstIconVisibleAndSecondHidden', () => {
+  let toBeVisibleMenuItem: Partial<MenuItem>;
+  let toBeHiddenMenuItem: Partial<MenuItem>;
+
+  it('should make the first item visible and the second item hidden', () => {
+    toBeVisibleMenuItem = { visible: false };
+    toBeHiddenMenuItem = { visible: true };
+    (Menu.getApplicationMenu as jest.Mock).mockImplementation(() => ({
+      getMenuItemById: jest.fn().mockImplementation((id) => {
+        if (id === 'toBeVisibleMenuItem') return toBeVisibleMenuItem;
+        if (id === 'toBeHiddenMenuItem') return toBeHiddenMenuItem;
+      }),
+    }));
+
+    makeFirstIconVisibleAndSecondHidden(
+      'toBeVisibleMenuItem',
+      'toBeHiddenMenuItem',
+    );
+
+    expect(toBeVisibleMenuItem.visible).toBe(true);
+    expect(toBeHiddenMenuItem.visible).toBe(false);
+  });
+});
