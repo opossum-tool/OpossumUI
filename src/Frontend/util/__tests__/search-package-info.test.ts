c05cff86436488152fdfdc167f246482c9cadcce
Markus Obendrauf <markusobendrauf@fb.com>
2023-10-04 16:12:30 +0200

    unit test searching
    Signed-off-by: Markus Obendrauf <markusobendrauf@fb.com>

---
+++ src/Frontend/util/__tests__/search-package-info.test.ts
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  licenseContainsSearchTerm,
+  packageInfoContainsSearchTerm,
+} from '../search-package-info';
+
+describe('packageInfoContainsSearchTerm', () => {
+  it('searches by package name', () => {
+    const testPackageInfo = {
+      packageName: 'Search_term package',
+      attributionIds: ['uuid1'],
+    };
+
+    expect(packageInfoContainsSearchTerm(testPackageInfo, 'SeArCh_TeRm')).toBe(
+      true,
+    );
+  });
+
+  it('searches by copyright', () => {
+    const testPackageInfo = {
+      copyright: '(c) Search_term 2022',
+      attributionIds: ['uuid2'],
+    };
+
+    expect(packageInfoContainsSearchTerm(testPackageInfo, 'SeArCh_TeRm')).toBe(
+      true,
+    );
+  });
+
+  it('searches by license name', () => {
+    const testPackageInfo = {
+      licenseName: 'Search_term licence',
+      attributionIds: ['uuid3'],
+    };
+
+    expect(packageInfoContainsSearchTerm(testPackageInfo, 'SeArCh_TeRm')).toBe(
+      true,
+    );
+  });
+
+  it('searches by package version', () => {
+    const testPackageInfo = {
+      packageVersion: 'version search_term',
+      attributionIds: ['uuid4'],
+    };
+
+    expect(packageInfoContainsSearchTerm(testPackageInfo, 'SeArCh_TeRm')).toBe(
+      true,
+    );
+  });
+
+  it('ignores other fields', () => {
+    const testPackageInfo = {
+      comments: ['comment search_term'],
+      licenseText: 'text search_term',
+      url: 'www.search_term.com',
+      attributionIds: ['uuid5'],
+    };
+
+    expect(packageInfoContainsSearchTerm(testPackageInfo, 'SeArCh_TeRm')).toBe(
+      false,
+    );
+  });
+});
+
+describe('licenseContainsSearchTerm', () => {
+  it('searches by license name', () => {
+    const testPackageInfo = {
+      licenseName: 'Search_term licence',
+      attributionIds: ['uuid3'],
+    };
+
+    expect(licenseContainsSearchTerm(testPackageInfo, 'SeArCh_TeRm')).toBe(
+      true,
+    );
+  });
+});
--- src/Frontend/util/search-package-info.ts
+++ src/Frontend/util/search-package-info.ts
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
-// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
 // SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
 //
 // SPDX-License-Identifier: Apache-2.0
