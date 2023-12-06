000c204eece7dcb1d0344ebf43b4c551a40abfdc
Maxim Stykow <maxim.stykow@tngtech.com>
2023-12-06 17:03:16 +0000

    feat: run tests with babel
    - faster and simpler: runs unit tests almost twice as fast
    
    Signed-off-by: Maxim Stykow <maxim.stykow@tngtech.com>

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   resetMocks: false,
   transformIgnorePatterns: ['/node_modules/(?!d3-*|internmap|axios)'],
-  transform: {
-    '/node_modules/(?!d3-*|internmap|axios)': 'babel-jest',
-    '^.+\\.tsx?$': ['ts-jest', { diagnostics: false }],
-  },
   setupFilesAfterEnv: ['./setupTests.ts'],
   watchAll: false,
   roots: ['<rootDir>/src/Frontend', '<rootDir>/src/ElectronBackend'],
