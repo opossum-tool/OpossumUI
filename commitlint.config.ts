62deec394370e082ffee3641201be116edd6f4d7
Maxim Stykow <maxim.stykow@tngtech.com>
2023-12-22 08:51:29 +0000

    chore: add commit linter
    - set up commitlint and enforce via commit hook
    - update contributing guide
    - add CI/CD check
    
    closes issue #2038
    
    Signed-off-by: Maxim Stykow <maxim.stykow@tngtech.com>

---
+++ commitlint.config.ts
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'trailer-exists': [2, 'always', 'Signed-off-by:'],
+  },
+};
+
+export default config;
