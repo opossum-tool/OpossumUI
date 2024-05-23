094c44275125d6a0fd5dbd47fabddf171a7a1838
Meret Behrens <meret.behrens@tngtech.com>
2024-05-23 17:13:00 +0200

    feat: use nivo responsive bar chart for progress and criticality bar
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>
    Signed-off-by: Maxim Stykow <maxim.stykow@tngtech.com>

--- src/Frontend/Components/ProgressBar/ProgressBar.util.tsx
+++ src/Frontend/Components/ProgressBar/ProgressBar.util.tsx
@@ -2,8 +2,8 @@
 // SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
 //
 // SPDX-License-Identifier: Apache-2.0
-import MuiBox from '@mui/material/Box';
-import { sum } from 'lodash';
+import MuiBox from '@mui/system/Box';
+import { CSSProperties } from 'react';
 
 import { criticalityColor, OpossumColors } from '../../shared-styles';
 import { navigateToSelectedPathOrOpenUnsavedPopup } from '../../state/actions/popup-actions/popup-actions';
@@ -37,27 +37,95 @@ export function useOnProgressBarClick(resourceIds: Array<string>) {
   };
 }
 
+const classes = {
+  tooltip: {
+    flex: 1,
+    background: 'rgba(97, 97, 97, 0.92)',
+    borderRadius: '4px',
+    color: 'white',
+    fontFamily: 'Karla Variable, sans-serif',
+    padding: 4,
+    fontSize: 10,
+    margin: 2,
+    fontWeight: 500,
+    position: 'relative',
+    top: 100,
+    'z-index': 100,
+  } as CSSProperties,
+  div: { display: 'flex', 'align-items': 'center' },
+  colorBlock: {
+    display: 'block',
+    width: 12,
+    height: 12,
+    marginRight: 7,
+    marginTop: 2,
+    marginLeft: 2,
+    marginBottom: 2,
+  },
+};
+
 export function getProgressBarTooltipText(
   progressBarData: ProgressBarData,
 ): React.ReactNode {
   return (
-    <MuiBox>
-      Number of resources…
-      <br />
-      …with attributions:{' '}
-      {new Intl.NumberFormat().format(
-        progressBarData.filesWithManualAttributionCount,
-      )}
-      <br />
-      …with only pre-selected attributions:{' '}
-      {new Intl.NumberFormat().format(
-        progressBarData.filesWithOnlyPreSelectedAttributionCount,
-      )}
-      <br />
-      …with only signals:{' '}
-      {new Intl.NumberFormat().format(
-        progressBarData.filesWithOnlyExternalAttributionCount,
-      )}
+    <MuiBox aria-label={'tooltip'} style={classes.tooltip}>
+      Number of resources...
+      <div style={classes.div}>
+        <span
+          style={{
+            ...classes.colorBlock,
+            background: OpossumColors.pastelDarkGreen,
+          }}
+        ></span>
+        <span>
+          ...with attributions:{' '}
+          {new Intl.NumberFormat().format(
+            progressBarData.filesWithManualAttributionCount,
+          )}
+        </span>
+      </div>
+      <div style={classes.div}>
+        <span
+          style={{
+            ...classes.colorBlock,
+            background: OpossumColors.pastelMiddleGreen,
+          }}
+        ></span>
+        <span>
+          …with only pre-selected attributions:{' '}
+          {new Intl.NumberFormat().format(
+            progressBarData.filesWithOnlyPreSelectedAttributionCount,
+          )}
+        </span>
+      </div>
+      <div style={classes.div}>
+        <span
+          style={{ ...classes.colorBlock, background: OpossumColors.pastelRed }}
+        ></span>
+        <span>
+          …with only signals:{' '}
+          {new Intl.NumberFormat().format(
+            progressBarData.filesWithOnlyExternalAttributionCount,
+          )}
+        </span>
+      </div>
+      <div style={classes.div}>
+        <span
+          style={{
+            ...classes.colorBlock,
+            background: OpossumColors.lightestBlue,
+          }}
+        ></span>
+        <span>
+          …without any signal or attribution:{' '}
+          {new Intl.NumberFormat().format(
+            progressBarData.fileCount -
+              (progressBarData.filesWithManualAttributionCount +
+                progressBarData.filesWithOnlyPreSelectedAttributionCount +
+                progressBarData.filesWithOnlyExternalAttributionCount),
+          )}
+        </span>
+      </div>
     </MuiBox>
   );
 }
@@ -70,129 +138,47 @@ export function getCriticalityBarTooltipText(
     progressBarData.filesWithHighlyCriticalExternalAttributionsCount -
     progressBarData.filesWithMediumCriticalExternalAttributionsCount;
   return (
-    <MuiBox>
+    <MuiBox aria-label={'tooltip'} style={classes.tooltip}>
       Number of resources with signals and no attributions…
-      <br />
-      …containing highly critical signals:{' '}
-      {new Intl.NumberFormat().format(
-        progressBarData.filesWithHighlyCriticalExternalAttributionsCount,
-      )}{' '}
-      <br />
-      …containing medium critical signals:{' '}
-      {new Intl.NumberFormat().format(
-        progressBarData.filesWithMediumCriticalExternalAttributionsCount,
-      )}{' '}
-      <br />
-      …containing only non-critical signals:{' '}
-      {new Intl.NumberFormat().format(filesWithNonCriticalExternalAttributions)}
+      <div style={classes.div} role="tooltip" aria-label="tooltip">
+        <span
+          style={{ ...classes.colorBlock, background: criticalityColor.high }}
+        ></span>
+        <span>
+          …containing highly critical signals:{' '}
+          {new Intl.NumberFormat().format(
+            progressBarData.filesWithHighlyCriticalExternalAttributionsCount,
+          )}{' '}
+        </span>
+      </div>
+      <div style={classes.div}>
+        <span
+          style={{
+            ...classes.colorBlock,
+            background: criticalityColor.medium,
+          }}
+        ></span>
+        <span>
+          …containing medium critical signals:{' '}
+          {new Intl.NumberFormat().format(
+            progressBarData.filesWithMediumCriticalExternalAttributionsCount,
+          )}{' '}
+        </span>
+      </div>
+      <div style={classes.div}>
+        <span
+          style={{
+            ...classes.colorBlock,
+            background: OpossumColors.lightestBlue,
+          }}
+        ></span>
+        <span>
+          …containing only non-critical signals:{' '}
+          {new Intl.NumberFormat().format(
+            filesWithNonCriticalExternalAttributions,
+          )}
+        </span>
+      </div>
     </MuiBox>
   );
-}
-
-export function getProgressBarBackground(
-  progressBarData: ProgressBarData,
-): string {
-  let filesWithManualAttributions: number =
-    (progressBarData.filesWithManualAttributionCount /
-      progressBarData.fileCount) *
-    100;
-  let filesWithOnlyPreselectedAttributions: number =
-    (progressBarData.filesWithOnlyPreSelectedAttributionCount /
-      progressBarData.fileCount) *
-    100;
-  let filesWithOnlyExternalAttributions: number =
-    (progressBarData.filesWithOnlyExternalAttributionCount /
-      progressBarData.fileCount) *
-    100;
-  let filesWithNothing: number =
-    100 -
-    filesWithManualAttributions -
-    filesWithOnlyPreselectedAttributions -
-    filesWithOnlyExternalAttributions;
-
-  [
-    filesWithManualAttributions,
-    filesWithOnlyPreselectedAttributions,
-    filesWithOnlyExternalAttributions,
-    filesWithNothing,
-  ] = roundToAtLeastOnePercentAndNormalize([
-    filesWithManualAttributions,
-    filesWithOnlyPreselectedAttributions,
-    filesWithOnlyExternalAttributions,
-    filesWithNothing,
-  ]);
-
-  const filesWithPreselectedOrManualAttributions: number =
-    filesWithManualAttributions + filesWithOnlyPreselectedAttributions;
-  const allFilesWithAttributions: number =
-    filesWithPreselectedOrManualAttributions +
-    filesWithOnlyExternalAttributions;
-
-  return (
-    'linear-gradient(to right,' +
-    ` ${OpossumColors.pastelDarkGreen} ${filesWithManualAttributions}%,` +
-    ` ${OpossumColors.pastelLightGreen} ${filesWithManualAttributions}%,` +
-    ` ${OpossumColors.pastelMiddleGreen} ${filesWithPreselectedOrManualAttributions}%,` +
-    ` ${OpossumColors.pastelRed} ${filesWithPreselectedOrManualAttributions}% ${allFilesWithAttributions}%,` +
-    ` ${OpossumColors.lightestBlue} ${allFilesWithAttributions}%)`
-  );
-}
-
-export function getCriticalityBarBackground(
-  progressBarData: ProgressBarData,
-): string {
-  if (progressBarData.filesWithOnlyExternalAttributionCount === 0) {
-    return `linear-gradient(to right, ${OpossumColors.pastelDarkGreen} 0% 100%)`;
-  }
-  let filesWithHighlyCriticalExternalAttributions =
-    (progressBarData.filesWithHighlyCriticalExternalAttributionsCount /
-      progressBarData.filesWithOnlyExternalAttributionCount) *
-    100;
-  let filesWithMediumCriticalExternalAttributions =
-    (progressBarData.filesWithMediumCriticalExternalAttributionsCount /
-      progressBarData.filesWithOnlyExternalAttributionCount) *
-    100;
-  let filesWithNonCriticalAttributions =
-    100 -
-    filesWithHighlyCriticalExternalAttributions -
-    filesWithMediumCriticalExternalAttributions;
-
-  [
-    filesWithHighlyCriticalExternalAttributions,
-    filesWithMediumCriticalExternalAttributions,
-    filesWithNonCriticalAttributions,
-  ] = roundToAtLeastOnePercentAndNormalize([
-    filesWithHighlyCriticalExternalAttributions,
-    filesWithMediumCriticalExternalAttributions,
-    filesWithNonCriticalAttributions,
-  ]);
-  const filesWithHighOrMediumCriticalExternalAttributions: number =
-    filesWithHighlyCriticalExternalAttributions +
-    filesWithMediumCriticalExternalAttributions;
-
-  return (
-    'linear-gradient(to right,' +
-    ` ${criticalityColor.high} ${filesWithHighlyCriticalExternalAttributions}%,` +
-    ` ${criticalityColor.medium} ${filesWithHighlyCriticalExternalAttributions}% ${filesWithHighOrMediumCriticalExternalAttributions}%,` +
-    ` ${OpossumColors.lightestBlue} ${filesWithHighOrMediumCriticalExternalAttributions}%)`
-  );
-}
-
-// We want to round everything > 0 to at least one percent so all possible segments of
-// the progress bar are always visible. For example, if there is only one file with
-// only signal left, we still want the user to see it even if there are 100,000
-// other files.
-// Only segments with 0 files should not be there.
-export function roundToAtLeastOnePercentAndNormalize(
-  numbers: Array<number>,
-): Array<number> {
-  const roundedNumbers = numbers.map((n) =>
-    n > 0 && n < 1 ? 1 : Math.round(n),
-  );
-  const differenceToExpectedSum = sum(roundedNumbers) - 100;
-  if (differenceToExpectedSum !== 0) {
-    const maxIdx = roundedNumbers.indexOf(Math.max(...roundedNumbers));
-    roundedNumbers[maxIdx] -= differenceToExpectedSum;
-  }
-  return roundedNumbers;
 }
