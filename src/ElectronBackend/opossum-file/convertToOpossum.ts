d07e9ebc68ef041b2fe13aa8bd65c83d61300638
Adrian Braemer <adrian.braemer@tngtech.com>
2025-01-28 16:07:44 +0100

    feat: new backend function wrapping the conversion of scancode to opossum

---
+++ src/ElectronBackend/opossum-file/convertToOpossum.ts
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+import { execFile as execFileCallback } from 'child_process';
+import { app } from 'electron';
+import { uniqueId } from 'lodash';
+import { join } from 'path';
+import { promisify } from 'util';
+
+import { getFilePathWithAppendix } from '../utils/getFilePathWithAppendix';
+
+const execFile = promisify(execFileCallback);
+
+const OPOSSUM_FILE_EXECUTABLE = './bin/opossum-file';
+
+function getTempPath(): string {
+  return join(app.getPath('temp'), uniqueId('opossum_'));
+}
+
+export async function convertScanCodeToOpossum(
+  pathToScanCode: string,
+  pathToOutput?: string,
+): Promise<string> {
+  const pathToOpossum = getFilePathWithAppendix(
+    pathToOutput ?? getTempPath(),
+    '.opossum',
+  );
+
+  await execFile(OPOSSUM_FILE_EXECUTABLE, [
+    'generate',
+    '-o',
+    pathToOpossum,
+    '--scan-code-json',
+    pathToScanCode,
+  ]);
+  return pathToOpossum;
+}
