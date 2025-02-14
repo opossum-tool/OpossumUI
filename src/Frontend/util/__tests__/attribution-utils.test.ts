21702bb1f7a5c54ea2ad2f6939fe8830055ab9d1
Philipp Martens <philipp.martens98@gmail.com>
2025-02-14 09:46:12 +0100

    refactor: clean up some unit tests

--- src/Frontend/util/__tests__/attribution-utils.test.ts
+++ src/Frontend/util/__tests__/attribution-utils.test.ts
@@ -442,19 +442,19 @@ describe('removeSlashesFromFilesWithChildren', () => {
 });
 
 describe('getBomAttributions', () => {
-  it('filters the correct BOM attributions', () => {
-    const testAttributions: Attributions = {
-      genericAttrib: { id: 'genericAttrib' },
-      firstPartyAttrib: { firstParty: true, id: 'firstPartyAttrib' },
-      followupAttrib: { followUp: true, id: 'followupAttrib' },
-      excludeAttrib: { excludeFromNotice: true, id: 'excludeAttrib' },
-      firstPartyExcludeAttrib: {
-        firstParty: true,
-        excludeFromNotice: true,
-        id: 'firstPartyExcludeAttrib',
-      },
-    };
+  const testAttributions: Attributions = {
+    genericAttrib: { id: 'genericAttrib' },
+    firstPartyAttrib: { firstParty: true, id: 'firstPartyAttrib' },
+    followupAttrib: { followUp: true, id: 'followupAttrib' },
+    excludeAttrib: { excludeFromNotice: true, id: 'excludeAttrib' },
+    firstPartyExcludeAttrib: {
+      firstParty: true,
+      excludeFromNotice: true,
+      id: 'firstPartyExcludeAttrib',
+    },
+  };
 
+  it('filters out attributions marked as follow up or first party', () => {
     const detailedBomAttributions = getBomAttributions(
       testAttributions,
       ExportType.DetailedBom,
@@ -463,7 +463,9 @@ describe('getBomAttributions', () => {
       genericAttrib: { id: 'genericAttrib' },
       excludeAttrib: { excludeFromNotice: true, id: 'excludeAttrib' },
     });
+  });
 
+  it('filters out attributions excluded from notice for compact BOM export', () => {
     const compactBomAttributions = getBomAttributions(
       testAttributions,
       ExportType.CompactBom,
@@ -471,37 +473,5 @@ describe('getBomAttributions', () => {
     expect(compactBomAttributions).toEqual<Attributions>({
       genericAttrib: { id: 'genericAttrib' },
     });
-
-    const completeTestAttributions: Attributions = {
-      completeAttrib: {
-        attributionConfidence: 1,
-        comment: 'Test',
-        packageName: 'Test component',
-        packageVersion: '',
-        packageNamespace: 'org.apache.xmlgraphics',
-        packageType: 'maven',
-        packagePURLAppendix:
-          '?repository_url=repo.spring.io/release#everybody/loves/dogs',
-        url: '',
-        copyright: '(c) John Doe',
-        licenseName: '',
-        licenseText: 'Permission is hereby granted, free of charge, to...',
-        originIds: [''],
-        preSelected: true,
-        id: 'completeAttrib',
-      },
-    };
-
-    const compactBomCompleteAttributions = getBomAttributions(
-      completeTestAttributions,
-      ExportType.CompactBom,
-    );
-    expect(compactBomCompleteAttributions).toEqual(completeTestAttributions);
-
-    const detailedBomCompleteAttributions = getBomAttributions(
-      completeTestAttributions,
-      ExportType.DetailedBom,
-    );
-    expect(detailedBomCompleteAttributions).toEqual(completeTestAttributions);
   });
 });
