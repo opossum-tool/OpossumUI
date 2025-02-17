a7b74d52d91293d2db9311d28729881d734058fb
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-02-17 08:20:39 +0100

    fix: add missing file
    Signed-off-by: Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>

--- src/ElectronBackend/main/menu/aboutMenu.ts
+++ src/ElectronBackend/main/menu/aboutMenu.ts
@@ -11,35 +11,43 @@ import {
   getPathOfNoticeDocument,
 } from '../notice-document-helpers';
 
+function getOpenOnGithub() {
+  return {
+    icon: getIconBasedOnTheme(
+      'icons/github-white.png',
+      'icons/github-black.png',
+    ),
+    label: 'Open on GitHub',
+    click: () =>
+      shell.openExternal('https://github.com/opossum-tool/opossumUI'),
+  };
+}
+
+function getOpossumUiNotices() {
+  return {
+    icon: getIconBasedOnTheme(
+      'icons/notice-white.png',
+      'icons/notice-black.png',
+    ),
+    label: 'OpossumUI Notices',
+    click: () => shell.openPath(getPathOfNoticeDocument()),
+  };
+}
+
+function getChromiumNotices() {
+  return {
+    icon: getIconBasedOnTheme(
+      'icons/chromium-white.png',
+      'icons/chromium-black.png',
+    ),
+    label: 'Chromium Notices',
+    click: () => shell.openPath(getPathOfChromiumNoticeDocument()),
+  };
+}
+
 export function getAboutMenu() {
   return {
     label: 'About',
-    submenu: [
-      {
-        icon: getIconBasedOnTheme(
-          'icons/github-white.png',
-          'icons/github-black.png',
-        ),
-        label: 'Open on GitHub',
-        click: () =>
-          shell.openExternal('https://github.com/opossum-tool/opossumUI'),
-      },
-      {
-        icon: getIconBasedOnTheme(
-          'icons/notice-white.png',
-          'icons/notice-black.png',
-        ),
-        label: 'OpossumUI Notices',
-        click: () => shell.openPath(getPathOfNoticeDocument()),
-      },
-      {
-        icon: getIconBasedOnTheme(
-          'icons/chromium-white.png',
-          'icons/chromium-black.png',
-        ),
-        label: 'Chromium Notices',
-        click: () => shell.openPath(getPathOfChromiumNoticeDocument()),
-      },
-    ],
+    submenu: [getOpenOnGithub(), getOpossumUiNotices(), getChromiumNotices()],
   };
 }
