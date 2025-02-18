289a064bc81c36b9532be4d5eaa815ee105bf0eb
Philipp Martens <philipp.martens98@gmail.com>
2025-02-18 14:04:19 +0100

    feat: fully integrate merge dialog with unsaved check mechanism

--- src/Frontend/state/actions/view-actions/types.ts
+++ src/Frontend/state/actions/view-actions/types.ts
@@ -24,6 +24,7 @@ export type ViewAction =
   | OpenPopupAction
   | SetOpenFileRequestAction
   | SetImportFileRequestAction
+  | SetMergeRequestAction
   | SetExportFileRequestAction;
 
 export interface ResetViewStateAction {
