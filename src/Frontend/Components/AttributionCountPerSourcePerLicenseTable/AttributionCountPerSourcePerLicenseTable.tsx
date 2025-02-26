73464a1f97cf3605e75d7118f574785edeed810d
Philipp Martens <philipp.martens98@gmail.com>
2025-02-26 15:14:01 +0100

    refactor: delete ProjectLicensesTable component
    * as there was only one remaining component it, move its functionality there

--- src/Frontend/Components/AttributionCountPerSourcePerLicenseTable/AttributionCountPerSourcePerLicenseTable.tsx
+++ src/Frontend/Components/AttributionCountPerSourcePerLicenseTable/AttributionCountPerSourcePerLicenseTable.tsx
@@ -5,7 +5,11 @@
 import MuiBox from '@mui/material/Box';
 import MuiTable from '@mui/material/Table';
 import MuiTableBody from '@mui/material/TableBody';
+import MuiTableCell from '@mui/material/TableCell';
 import MuiTableContainer from '@mui/material/TableContainer';
+import MuiTableFooter from '@mui/material/TableFooter';
+import MuiTableHead from '@mui/material/TableHead';
+import MuiTableRow from '@mui/material/TableRow';
 import MuiTypography from '@mui/material/Typography';
 
 import {
@@ -16,6 +20,7 @@ import {
 import { AttributionCountPerSourcePerLicenseTableFooter } from './AttributionCountPerSourcePerLicenseTableFooter/AttributionCountPerSourcePerLicenseTableFooter';
 import { AttributionCountPerSourcePerLicenseTableHead } from './AttributionCountPerSourcePerLicenseTableHead/AttributionCountPerSourcePerLicenseTableHead';
 import { AttributionCountPerSourcePerLicenseTableRow } from './AttributionCountPerSourcePerLicenseTableRow/AttributionCountPerSourcePerLicenseTableRow';
+import { tableClasses } from '../../shared-styles';
 
 const classes = {
   container: {
@@ -38,46 +43,117 @@ export const AttributionCountPerSourcePerLicenseTable: React.FC<
     props.licenseCounts.totalAttributionsPerSource,
   );
 
+  const totalNumberOfAttributionsPerSource = sourceNames.map((sourceName) =>
+    props.licenseCounts.totalAttributionsPerSource[sourceName].toString(),
+  );
+  const totalNumberOfAttributions = Object.values(
+    props.licenseCounts.totalAttributionsPerSource,
+  )
+    .reduce((partialSum, num) => partialSum + num, 0)
+    .toString();
+
+  const footerRow = [FOOTER_TITLE]
+    .concat(totalNumberOfAttributionsPerSource)
+    .concat(totalNumberOfAttributions);
+  const headerRow = [LICENSE_COLUMN_NAME_IN_TABLE]
+    .concat(sourceNames)
+    .concat(TOTAL_SOURCES_TITLE)
+    .map(
+      (sourceName) => sourceName.charAt(0).toUpperCase() + sourceName.slice(1),
+    );
+
+  Object.entries(
+    props.licenseCounts.attributionCountPerSourcePerLicense,
+  ).forEach(
+    ([licenseName, value]) =>
+      (value[TOTAL_SOURCES_TITLE] =
+        props.licenseCounts.totalAttributionsPerLicense[licenseName]),
+  );
+
+  const tableHead = (
+    <MuiTableHead>
+      <MuiTableRow>
+        {headerRow.map((columnHeader, columnIndex) => (
+          <MuiTableCell
+            sx={tableClasses.head}
+            key={columnIndex}
+            align={columnIndex === 0 ? 'left' : 'center'}
+          >
+            {columnHeader}
+          </MuiTableCell>
+        ))}
+      </MuiTableRow>
+    </MuiTableHead>
+  );
+
+  const buildTableRow = (licenseName: string, rowIndex: number) => {
+    const licenseNameCell = (
+      <MuiTableCell
+        sx={{
+          ...tableClasses.body,
+        }}
+        key={0}
+        align={'left'}
+      >
+        <span>{licenseName}</span>
+      </MuiTableCell>
+    );
+
+    const countBySourceCells = sourceNames
+      .concat(TOTAL_SOURCES_TITLE)
+      .map((sourceName, sourceIdx) => {
+        const columnIndex = 1 + sourceIdx;
+
+        return (
+          <MuiTableCell
+            sx={{
+              ...tableClasses.body,
+            }}
+            key={columnIndex}
+            align={'center'}
+          >
+            {props.licenseCounts.attributionCountPerSourcePerLicense[
+              licenseName
+            ][sourceName] || '-'}
+          </MuiTableCell>
+        );
+      });
+
+    return (
+      <MuiTableRow key={rowIndex}>
+        {[licenseNameCell, ...countBySourceCells]}
+      </MuiTableRow>
+    );
+  };
+
+  const tableFooter = (
+    <MuiTableFooter>
+      <MuiTableRow>
+        {footerRow.map((total, columnIndex) => (
+          <MuiTableCell
+            sx={tableClasses.footer}
+            key={columnIndex}
+            align={columnIndex === 0 ? 'left' : 'center'}
+          >
+            {total}
+          </MuiTableCell>
+        ))}
+      </MuiTableRow>
+    </MuiTableFooter>
+  );
+
   return (
     <MuiBox>
       <MuiTypography variant="subtitle1">{props.title}</MuiTypography>
       <MuiTableContainer sx={classes.container}>
         <MuiTable size="small" stickyHeader>
-          <AttributionCountPerSourcePerLicenseTableHead
-            sourceNames={sourceNames}
-          />
+          {tableHead}
           <MuiTableBody>
             {Object.keys(props.licenseNamesWithCriticality)
               .toSorted()
-              .map((licenseName, rowIndex) => (
-                <AttributionCountPerSourcePerLicenseTableRow
-                  sourceNames={sourceNames}
-                  signalCountsPerSource={
-                    props.licenseCounts.attributionCountPerSourcePerLicense[
-                      licenseName
-                    ]
-                  }
-                  licenseName={licenseName}
-                  licenseCriticality={
-                    props.licenseNamesWithCriticality[licenseName]
-                  }
-                  licenseClassification={
-                    props.licenseNamesWithClassification[licenseName]
-                  }
-                  totalSignalCount={
-                    props.licenseCounts.totalAttributionsPerLicense[licenseName]
-                  }
-                  key={rowIndex}
-                  rowIndex={rowIndex}
-                />
-              ))}
+              .map(buildTableRow)}
           </MuiTableBody>
-          <AttributionCountPerSourcePerLicenseTableFooter
-            sourceNames={sourceNames}
-            totalAttributionsPerSource={
-              props.licenseCounts.totalAttributionsPerSource
-            }
-          />
+          {tableFooter}
         </MuiTable>
       </MuiTableContainer>
     </MuiBox>
