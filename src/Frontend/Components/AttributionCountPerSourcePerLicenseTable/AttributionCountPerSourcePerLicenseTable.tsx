57314542528b6c3dc470385b84794eb32d02863b
Philipp Martens <philipp.martens98@gmail.com>
2025-02-26 15:14:01 +0100

    refactor: delete ProjectLicensesTable component
    * as there was only one remaining component it, move its functionality there

--- src/Frontend/Components/AttributionCountPerSourcePerLicenseTable/AttributionCountPerSourcePerLicenseTable.tsx
+++ src/Frontend/Components/AttributionCountPerSourcePerLicenseTable/AttributionCountPerSourcePerLicenseTable.tsx
@@ -2,10 +2,22 @@
 // SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
 //
 // SPDX-License-Identifier: Apache-2.0
-import { sortBy } from 'lodash';
+import MuiBox from '@mui/material/Box';
+import MuiTable from '@mui/material/Table';
+import MuiTableBody from '@mui/material/TableBody';
+import MuiTableCell from '@mui/material/TableCell';
+import MuiTableContainer from '@mui/material/TableContainer';
+import MuiTableFooter from '@mui/material/TableFooter';
+import MuiTableHead from '@mui/material/TableHead';
+import MuiTableRow from '@mui/material/TableRow';
+import MuiTypography from '@mui/material/Typography';
 
-import { LicenseCounts, LicenseNamesWithCriticality } from '../../types/types';
-import { ProjectLicensesTable } from '../ProjectLicensesTable/ProjectLicensesTable';
+import { tableClasses } from '../../shared-styles';
+import {
+  LicenseCounts,
+  LicenseNamesWithClassification,
+  LicenseNamesWithCriticality,
+} from '../../types/types';
 
 const classes = {
   container: {
@@ -21,6 +33,7 @@ const TOTAL_SOURCES_TITLE = 'Total';
 interface AttributionCountPerSourcePerLicenseTableProps {
   licenseCounts: LicenseCounts;
   licenseNamesWithCriticality: LicenseNamesWithCriticality;
+  licenseNamesWithClassification: LicenseNamesWithClassification;
   title: string;
 }
 
@@ -58,16 +71,92 @@ export const AttributionCountPerSourcePerLicenseTable: React.FC<
         props.licenseCounts.totalAttributionsPerLicense[licenseName]),
   );
 
+  const tableHead = (
+    <MuiTableHead>
+      <MuiTableRow>
+        {headerRow.map((columnHeader, columnIndex) => (
+          <MuiTableCell
+            sx={tableClasses.head}
+            key={columnIndex}
+            align={columnIndex === 0 ? 'left' : 'center'}
+          >
+            {columnHeader}
+          </MuiTableCell>
+        ))}
+      </MuiTableRow>
+    </MuiTableHead>
+  );
+
+  const buildTableRow = (licenseName: string, rowIndex: number) => {
+    const licenseNameCell = (
+      <MuiTableCell
+        sx={{
+          ...tableClasses.body,
+        }}
+        key={0}
+        align={'left'}
+      >
+        <span>{licenseName}</span>
+      </MuiTableCell>
+    );
+
+    const countBySourceCells = sourceNames
+      .concat(TOTAL_SOURCES_TITLE)
+      .map((sourceName, sourceIdx) => {
+        const columnIndex = 1 + sourceIdx;
+
+        return (
+          <MuiTableCell
+            sx={{
+              ...tableClasses.body,
+            }}
+            key={columnIndex}
+            align={'center'}
+          >
+            {props.licenseCounts.attributionCountPerSourcePerLicense[
+              licenseName
+            ][sourceName] || '-'}
+          </MuiTableCell>
+        );
+      });
+
+    return (
+      <MuiTableRow key={rowIndex}>
+        {[licenseNameCell, ...countBySourceCells]}
+      </MuiTableRow>
+    );
+  };
+
+  const tableFooter = (
+    <MuiTableFooter>
+      <MuiTableRow>
+        {footerRow.map((total, columnIndex) => (
+          <MuiTableCell
+            sx={tableClasses.footer}
+            key={columnIndex}
+            align={columnIndex === 0 ? 'left' : 'center'}
+          >
+            {total}
+          </MuiTableCell>
+        ))}
+      </MuiTableRow>
+    </MuiTableFooter>
+  );
+
   return (
-    <ProjectLicensesTable
-      title={props.title}
-      containerStyle={classes.container}
-      columnHeaders={headerRow}
-      columnNames={headerRow}
-      rowNames={sortBy(Object.keys(props.licenseNamesWithCriticality))}
-      tableContent={props.licenseCounts.attributionCountPerSourcePerLicense}
-      tableFooter={footerRow}
-      licenseNamesWithCriticality={props.licenseNamesWithCriticality}
-    />
+    <MuiBox>
+      <MuiTypography variant="subtitle1">{props.title}</MuiTypography>
+      <MuiTableContainer sx={classes.container}>
+        <MuiTable size="small" stickyHeader>
+          {tableHead}
+          <MuiTableBody>
+            {Object.keys(props.licenseNamesWithCriticality)
+              .toSorted()
+              .map(buildTableRow)}
+          </MuiTableBody>
+          {tableFooter}
+        </MuiTable>
+      </MuiTableContainer>
+    </MuiBox>
   );
 };
