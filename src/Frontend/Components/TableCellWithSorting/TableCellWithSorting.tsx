68361bb9a320f1f638ec6d375ecf2e0c28faf1e4
Philipp Martens <philipp.martens98@gmail.com>
2025-02-28 15:54:37 +0100

    feat: introduce sort functionality on signals per sources table

---
+++ src/Frontend/Components/TableCellWithSorting/TableCellWithSorting.tsx
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: Meta Platforms, Inc. and its affiliates
+// SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+import { TableCellProps, TableSortLabel } from '@mui/material';
+import MuiBox from '@mui/material/Box';
+import MuiTableCell from '@mui/material/TableCell';
+import { visuallyHidden } from '@mui/utils';
+
+export type Order = 'asc' | 'desc';
+
+interface TableCellWithSortingProps extends TableCellProps {
+  order: Order;
+  isSortedColumn: boolean;
+  onRequestSort: () => void;
+}
+
+export const TableCellWithSorting: React.FC<TableCellWithSortingProps> = (
+  props,
+) => {
+  return (
+    <MuiTableCell
+      {...props}
+      sx={{
+        '.Mui-active': { color: 'white' },
+        '& :hover': { color: 'white !important' },
+        ...props.sx,
+      }}
+      sortDirection={props.isSortedColumn ? props.order : false}
+    >
+      <TableSortLabel
+        sx={{
+          '& .MuiTableSortLabel-icon': { color: 'white !important' },
+        }}
+        active={props.isSortedColumn}
+        direction={props.isSortedColumn ? props.order : 'asc'}
+        onClick={props.onRequestSort}
+      >
+        {props.children}
+        {props.isSortedColumn ? (
+          <MuiBox component="span" sx={visuallyHidden}>
+            {`sorted ${props.order}ending`}
+          </MuiBox>
+        ) : null}
+      </TableSortLabel>
+    </MuiTableCell>
+  );
+};
