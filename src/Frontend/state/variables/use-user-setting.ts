bc4dee447f48662133058dde6c70908f9faadc0c
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-03-18 09:48:04 +0100

    chore: keep state in sync with backend
    Signed-off-by: Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>

--- src/Frontend/state/variables/use-user-setting.ts
+++ src/Frontend/state/variables/use-user-setting.ts
@@ -7,8 +7,14 @@ import { DependencyList, useCallback, useEffect } from 'react';
 
 import { AllowedFrontendChannels } from '../../../shared/ipc-channels';
 import { UserSettings } from '../../../shared/shared-types';
-import { useIpcRenderer } from '../../util/use-ipc-renderer';
-import { fetchUserSettings } from '../actions/user-settings-actions/user-settings-actions';
+import {
+  useIpcRenderer,
+  UserSettingsChangedListener,
+} from '../../util/use-ipc-renderer';
+import {
+  fetchUserSettings,
+  setUserSetting,
+} from '../actions/user-settings-actions/user-settings-actions';
 import { useAppDispatch } from '../hooks';
 import { useVariable } from './use-variable';
 
@@ -94,9 +100,17 @@ export function useUserSetting<T extends keyof UserSettings>(
   return [storedValue, setStoredValue, hydrated];
 }
 
+// should only be called once
 export function useInitialSyncUserSettings() {
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(fetchUserSettings());
   }, [dispatch]);
+
+  useIpcRenderer<UserSettingsChangedListener>(
+    AllowedFrontendChannels.UserSettingsChanged,
+    (_, updatedSettings: Partial<UserSettings>) =>
+      dispatch(setUserSetting(updatedSettings)),
+    [dispatch],
+  );
 }
