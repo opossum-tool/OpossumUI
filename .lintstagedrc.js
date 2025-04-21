42ec4200f4c498e0c1fba199406ad4168b94f8b6
Maxim Stykow <maxim.stykow@tngtech.com>
2025-04-21 22:01:47 +0200

    chore: fix knip not running in pre-commit hook
    Signed-off-by: Maxim Stykow <maxim.stykow@tngtech.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,8 +4,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
-  '*': [() => 'yarn copyright-lint-check', 'prettier --write --ignore-unknown'],
-  '*': [() => 'knip'],
+  '*': [
+    () => 'yarn copyright-lint-check',
+    () => 'knip',
+    'prettier --write --ignore-unknown',
+  ],
   '*.{ts,tsx}': 'eslint',
   '!(src/ElectronBackend/**/*)*.{ts,tsx}': () => 'tsc -p ./',
   'src/ElectronBackend/**/*.{ts,tsx}': () =>
