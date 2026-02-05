d9e54f43ccfdb629550330d98df32fd9f5a9892d
Marius Asadauskas <marius.asadauskas@tngtech.com>
2026-02-05 09:02:38 +0100

    fix: refactor setup to use vitest spyon

--- src/testing/setup.ts
+++ src/testing/setup.ts
@@ -11,43 +11,6 @@ import { DEFAULT_USER_SETTINGS } from '../shared/shared-constants';
 import { ElectronAPI } from '../shared/shared-types';
 import { faker } from './Faker';
 
-// We suppress the recharts warning that is due to our mocking in tests.
-const SUBSTRINGS_TO_SUPPRESS_IN_CONSOLE_WARN = [
-  'of chart should be greater than 0,',
-];
-
-// this is a quick fix for #938
-const SUBSTRINGS_TO_SUPPRESS_IN_CONSOLE_ERROR = [
-  'should be wrapped into act(...)',
-];
-
-// Surpress more logs, so that the test output is more readable.
-vi.mock('../../ElectronBackend/main/ProcessingStatusUpdater.ts', () => ({
-  ProcessingStatusUpdater: vi.fn().mockImplementation(() => ({
-    info: vi.fn(),
-    warn: vi.fn(),
-    error: vi.fn(),
-    startProcessing: vi.fn(),
-    endProcessing: vi.fn(),
-  })),
-}));
-
-vi.mock('electron-log', () => ({
-  default: {
-    info: vi.fn(),
-    warn: vi.fn(),
-    error: vi.fn(),
-  },
-}));
-
-vi.mock('../ElectronBackend/main/logger.ts', () => ({
-  default: {
-    info: vi.fn(),
-    warn: vi.fn(),
-    error: vi.fn(),
-  },
-}));
-
 class ResizeObserver {
   observe = noop;
   unobserve = noop;
@@ -75,44 +38,57 @@ global.window.electronAPI = {
   api: vi.fn().mockImplementation(executeCommand),
 } satisfies ElectronAPI;
 
-const originalConsoleError = console.error;
-const originalConsoleWarn = console.warn;
-const originalConsoleInfo = console.info;
-
-console.warn = getMockConsoleImplementation(
-  SUBSTRINGS_TO_SUPPRESS_IN_CONSOLE_WARN,
-  'warn',
-);
-console.error = getMockConsoleImplementation(
-  SUBSTRINGS_TO_SUPPRESS_IN_CONSOLE_ERROR,
-  'error',
-);
 faker.packageSearch.usePackageNames();
 faker.packageSearch.usePackageNamespaces();
 faker.packageSearch.usePackageVersions();
 
-function getMockConsoleImplementation(
-  selectors: Array<string>,
-  type: 'info' | 'warn' | 'error',
-) {
-  return (...args: Array<unknown>): void => {
-    if (
-      selectors.filter(
-        (selector) => typeof args[0] === 'string' && args[0].includes(selector),
-      ).length
-    ) {
-      return;
-    }
-    switch (type) {
-      case 'error': {
-        return originalConsoleError.call(console, args);
-      }
-      case 'warn': {
-        return originalConsoleWarn.call(console, args);
-      }
-      case 'info': {
-        return originalConsoleInfo.call(console, args);
-      }
-    }
-  };
+// Suppress specific console logs to make the tests cleaner.
+interface ConsoleType {
+  type: 'error' | 'warn';
+  suppressedMessage: string;
+  original: (...args: Array<unknown>) => void;
 }
+for (const { type, suppressedMessage, original } of [
+  {
+    type: 'error',
+    suppressedMessage: 'should be wrapped into act(...)',
+    original: console.error,
+  },
+  {
+    type: 'warn',
+    suppressedMessage: 'of chart should be greater than 0,',
+    original: console.warn,
+  },
+] satisfies Array<ConsoleType>) {
+  vi.spyOn(console, type).mockImplementation((...args: Array<unknown>) => {
+    if (!(typeof args[0] === 'string' && args[0].includes(suppressedMessage))) {
+      original(...args);
+    }
+  });
+}
+
+vi.mock('../../ElectronBackend/main/ProcessingStatusUpdater.ts', () => ({
+  ProcessingStatusUpdater: vi.fn().mockImplementation(() => ({
+    info: vi.fn(),
+    warn: vi.fn(),
+    error: vi.fn(),
+    startProcessing: vi.fn(),
+    endProcessing: vi.fn(),
+  })),
+}));
+
+vi.mock('electron-log', () => ({
+  default: {
+    info: vi.fn(),
+    warn: vi.fn(),
+    error: vi.fn(),
+  },
+}));
+
+vi.mock('../ElectronBackend/main/logger.ts', () => ({
+  default: {
+    info: vi.fn(),
+    warn: vi.fn(),
+    error: vi.fn(),
+  },
+}));
