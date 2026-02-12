66598ac10a107d6714962f1a3297f749a47206bd
Leon May <leon.may@tngtech.com>
2026-02-12 16:20:41 +0100

    feat: migrate filter button to api counts

--- src/Frontend/Components/AttributionPanels/SignalsPanel/SignalsPanel.tsx
+++ src/Frontend/Components/AttributionPanels/SignalsPanel/SignalsPanel.tsx
@@ -5,20 +5,40 @@
 import MuiBox from '@mui/material/Box';
 
 import { SIGNAL_FILTERS } from '../../../shared-constants';
+import { useAppSelector } from '../../../state/hooks';
+import { getSelectedResourceId } from '../../../state/selectors/resource-selectors';
 import { useAttributionIdsForReplacement } from '../../../state/variables/use-attribution-ids-for-replacement';
 import { useFilteredSignals } from '../../../state/variables/use-filtered-data';
+import { useUserSettings } from '../../../state/variables/use-user-setting';
+import { backend } from '../../../util/backendClient';
 import { PackagesPanel } from '../PackagesPanel/PackagesPanel';
 import { DeleteButton } from './DeleteButton/DeleteButton';
-import { IncludeExcludeButton } from './IncludeExcludeButton/IncludeExcludeButton';
 import { LinkButton } from './LinkButton/LinkButton';
 import { RestoreButton } from './RestoreButton/RestoreButton';
 import { SignalsList } from './SignalsList/SignalsList';
+import { ToggleHiddenSignalsButton } from './ToggleHiddenSignalsButton/ToggleHiddenSignalsButton';
 
 export function SignalsPanel() {
   const [attributionIdsForReplacement] = useAttributionIdsForReplacement();
+  const selectedResourceId = useAppSelector(getSelectedResourceId);
+
+  const [userSettings, updateUserSettings] = useUserSettings();
+  const areHiddenSignalsVisible = userSettings.areHiddenSignalsVisible;
+
+  const [{ filters, search, selectedLicense }] = useFilteredSignals();
+
+  const counts = backend.filterCounts.useQuery({
+    external: true,
+    filters,
+    search,
+    license: selectedLicense,
+    resourcePathForRelationships: selectedResourceId,
+    showResolved: areHiddenSignalsVisible,
+  });
 
   return (
     <PackagesPanel
+      filterCounts={counts.data?.sameOrDescendant}
       availableFilters={SIGNAL_FILTERS}
       disableSelectAll={!!attributionIdsForReplacement.length}
       renderActions={(props) => (
@@ -27,7 +47,14 @@ export function SignalsPanel() {
           <DeleteButton {...props} />
           <RestoreButton {...props} />
           <MuiBox flex={1} />
-          <IncludeExcludeButton />
+          <ToggleHiddenSignalsButton
+            showHiddenSignals={areHiddenSignalsVisible}
+            setShowHiddenSignals={(showHiddenSignals) =>
+              updateUserSettings(() => ({
+                areHiddenSignalsVisible: showHiddenSignals,
+              }))
+            }
+          />
         </>
       )}
       useFilteredData={useFilteredSignals}
