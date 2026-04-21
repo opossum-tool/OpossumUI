b7aed9513d84b8be79d0fa0ecc119f54050dabae
Leon May <leon.may@tngtech.com>
2026-04-21 19:18:04 +0200

    fix: drive-by to remove unused actions we forgot before

--- src/Frontend/state/actions/resource-actions/types.ts
+++ src/Frontend/state/actions/resource-actions/types.ts
@@ -3,8 +3,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 import {
-  type AttributionData,
-  type BaseUrlsForSources,
   type PackageInfo,
   type ProjectMetadata,
 } from '../../../../shared/shared-types';
@@ -12,8 +10,6 @@ import {
 export const ACTION_SET_SELECTED_ATTRIBUTION_ID =
   'ACTION_SET_SELECTED_ATTRIBUTION_ID';
 export const ACTION_RESET_RESOURCE_STATE = 'ACTION_RESET_RESOURCE_STATE';
-export const ACTION_SET_MANUAL_ATTRIBUTION_DATA =
-  'ACTION_SET_MANUAL_ATTRIBUTION_DATA';
 export const ACTION_SET_TEMPORARY_PACKAGE_INFO =
   'ACTION_SET_TEMPORARY_PACKAGE_INFO';
 export const ACTION_SET_SELECTED_RESOURCE_ID =
@@ -21,62 +17,25 @@ export const ACTION_SET_SELECTED_RESOURCE_ID =
 export const ACTION_SET_EXPANDED_IDS = 'ACTION_SET_EXPANDED_IDS';
 export const ACTION_SET_TARGET_SELECTED_RESOURCE_ID =
   'ACTION_SET_TARGET_SELECTED_RESOURCE_ID';
-export const ACTION_SET_FILES_WITH_CHILDREN = 'ACTION_SET_FILES_WITH_CHILDREN';
-export const ACTION_UPDATE_ATTRIBUTION =
-  'ACTION_UPDATE_ATTRIBUTION_FOR_SELECTED';
-export const ACTION_CREATE_ATTRIBUTION_FOR_SELECTED_RESOURCE =
-  'ACTION_CREATE_ATTRIBUTION_FOR_SELECTED_RESOURCE';
-export const ACTION_DELETE_ATTRIBUTION =
-  'ACTION_DELETE_ATTRIBUTION_FOR_SELECTED';
 export const ACTION_SET_TARGET_SELECTED_ATTRIBUTION_ID =
   'ACTION_SET_TARGET_SELECTED_ATTRIBUTION_ID';
-export const ACTION_SET_RESOLVED_EXTERNAL_ATTRIBUTIONS =
-  'ACTION_SET_RESOLVED_EXTERNAL_ATTRIBUTIONS';
-export const ACTION_ADD_RESOLVED_EXTERNAL_ATTRIBUTIONS =
-  'ACTION_ADD_RESOLVED_EXTERNAL_ATTRIBUTIONS';
-export const ACTION_REPLACE_ATTRIBUTION_WITH_MATCHING =
-  'ACTION_REPLACE_ATTRIBUTION_WITH_MATCHING';
-export const ACTION_LINK_TO_ATTRIBUTION = 'ACTION_LINK_TO_ATTRIBUTION';
-export const ACTION_UNLINK_RESOURCE_FROM_ATTRIBUTION =
-  'ACTION_UNLINK_RESOURCE_FROM_ATTRIBUTION';
-export const ACTION_REMOVE_RESOLVED_EXTERNAL_ATTRIBUTIONS =
-  'ACTION_REMOVE_RESOLVED_EXTERNAL_ATTRIBUTIONS';
 export const ACTION_SET_PROJECT_METADATA = 'ACTION_SET_PROJECT_METADATA';
-export const ACTION_SET_BASE_URLS_FOR_SOURCES =
-  'ACTION_SET_BASE_URLS_FOR_SOURCES';
 export const ACTION_SET_IS_PACKAGE_INFO_DIRTY =
   'ACTION_SET_IS_PACKAGE_INFO_DIRTY';
 
 export type ResourceAction =
   | ResetResourceStateAction
-  | SetManualDataAction
   | SetTemporaryDisplayPackageInfoAction
   | SetSelectedResourceIdAction
   | SetExpandedIdsAction
   | SetTargetSelectedResourceId
   | SetSelectedAttributionId
-  | SetFilesWithChildren
-  | UpdateAttribution
-  | DeleteAttribution
-  | CreateAttributionForSelectedResource
   | SetTargetSelectedAttributionIdAction
-  | ReplaceAttributionWithMatchingAttributionAction
-  | LinkToAttributionAction
-  | UnlinkResourceFromAttributionAction
-  | SetResolvedExternalAttributions
-  | AddResolvedExternalAttributions
-  | RemoveResolvedExternalAttributions
   | SetProjectMetadata
-  | SetBaseUrlsForSources
   | SetIsPackageInfoDirtyAction;
 
 export interface ResetResourceStateAction {
   type: typeof ACTION_RESET_RESOURCE_STATE;
-}
-
-export interface SetManualDataAction {
-  type: typeof ACTION_SET_MANUAL_ATTRIBUTION_DATA;
-  payload: AttributionData;
 }
 
 export interface SetTemporaryDisplayPackageInfoAction {
@@ -109,84 +68,9 @@ export interface SetTargetSelectedAttributionIdAction {
   payload: string | null;
 }
 
-export interface SetFilesWithChildren {
-  type: typeof ACTION_SET_FILES_WITH_CHILDREN;
-  payload: Set<string>;
-}
-
-export interface CreateAttributionForSelectedResource {
-  type: typeof ACTION_CREATE_ATTRIBUTION_FOR_SELECTED_RESOURCE;
-  payload: {
-    attributionId: string;
-    packageInfo: PackageInfo;
-  };
-}
-
-export interface UpdateAttribution {
-  type: typeof ACTION_UPDATE_ATTRIBUTION;
-  payload: {
-    attributionId: string;
-    packageInfo: PackageInfo;
-    jumpToUpdatedAttribution: boolean;
-  };
-}
-
-export interface DeleteAttribution {
-  type: typeof ACTION_DELETE_ATTRIBUTION;
-  payload: {
-    attributionId: string;
-    attributionBreakpoints: Set<string>;
-  };
-}
-
-export interface ReplaceAttributionWithMatchingAttributionAction {
-  type: typeof ACTION_REPLACE_ATTRIBUTION_WITH_MATCHING;
-  payload: {
-    attributionIdToReplace: string;
-    attributionIdToReplaceWith: string;
-    jumpToMatchingAttribution: boolean;
-    attributionBreakpoints: Set<string>;
-  };
-}
-
-export interface LinkToAttributionAction {
-  type: typeof ACTION_LINK_TO_ATTRIBUTION;
-  payload: {
-    resourceId: string;
-    attributionId: string;
-    jumpToMatchingAttribution: boolean;
-    attributionBreakpoints: Set<string>;
-  };
-}
-
-export interface UnlinkResourceFromAttributionAction {
-  type: typeof ACTION_UNLINK_RESOURCE_FROM_ATTRIBUTION;
-  payload: { resourceId: string; attributionId: string };
-}
-
-export interface SetResolvedExternalAttributions {
-  type: typeof ACTION_SET_RESOLVED_EXTERNAL_ATTRIBUTIONS;
-  payload: Set<string>;
-}
-
-export interface AddResolvedExternalAttributions {
-  type: typeof ACTION_ADD_RESOLVED_EXTERNAL_ATTRIBUTIONS;
-  payload: Array<string>;
-}
-
-export interface RemoveResolvedExternalAttributions {
-  type: typeof ACTION_REMOVE_RESOLVED_EXTERNAL_ATTRIBUTIONS;
-  payload: Array<string>;
-}
-
 export interface SetProjectMetadata {
   type: typeof ACTION_SET_PROJECT_METADATA;
   payload: ProjectMetadata;
-}
-
-export interface SetBaseUrlsForSources {
-  type: typeof ACTION_SET_BASE_URLS_FOR_SOURCES;
-  payload: BaseUrlsForSources;
 }
 
 export interface SetIsPackageInfoDirtyAction {
