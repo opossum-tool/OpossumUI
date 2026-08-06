0256bc4510c09c6eb0f348f4c2af524f0dd59d04
Philipp Karcher <philipp.karcher@tngtech.com>
2026-08-06 10:31:17 +0200

    fix: reduce flickering
    Signed-off-by: Philipp Karcher <philipp.karcher@tngtech.com>

--- src/Frontend/Components/SplitDialog/SplitDialog.tsx
+++ src/Frontend/Components/SplitDialog/SplitDialog.tsx
@@ -3,6 +3,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 import MuiAlert from '@mui/material/Alert';
+import MuiCollapse from '@mui/material/Collapse';
 import MuiLinearProgress from '@mui/material/LinearProgress';
 import MuiTypography from '@mui/material/Typography';
 import { useEffect, useState } from 'react';
@@ -128,20 +129,24 @@ export const SplitDialog: React.FC<SplitDialogProps> = ({
           testId={'split-destination-path'}
           disabled={splitInProgress || selectedResourcePaths.length === 0}
         />
-        {splitInProgress ? (
+        <MuiCollapse
+          in={Boolean(splitInProgress || splitSucceeded || errorMessage)}
+        >
           <div style={{ marginTop: '20px' }}>
-            <MuiTypography>{text.splitDialog.inProgress}</MuiTypography>
-            <MuiLinearProgress sx={{ marginTop: '8px' }} />
+            {splitInProgress ? (
+              <>
+                <MuiTypography>{text.splitDialog.inProgress}</MuiTypography>
+                <MuiLinearProgress sx={{ marginTop: '8px' }} />
+              </>
+            ) : splitSucceeded ? (
+              <MuiAlert severity={'success'}>
+                {text.splitDialog.success}
+              </MuiAlert>
+            ) : errorMessage ? (
+              <MuiAlert severity={'error'}>{errorMessage}</MuiAlert>
+            ) : null}
           </div>
-        ) : splitSucceeded ? (
-          <MuiAlert severity={'success'} sx={{ marginTop: '20px' }}>
-            {text.splitDialog.success}
-          </MuiAlert>
-        ) : errorMessage ? (
-          <MuiAlert severity={'error'} sx={{ marginTop: '20px' }}>
-            {errorMessage}
-          </MuiAlert>
-        ) : null}
+        </MuiCollapse>
       </div>
     </NotificationPopup>
   );
